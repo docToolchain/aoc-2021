0e361aee1a6428a67d5b0b62d49ab6eb366427bd
aspik79 <cj_guard@yahoo.com>
2021-12-01 08:55:16 +0100

    Add files via upload

---
+++ day01/typescript/aspik79/sonarSweeper.ts
@@ -0,0 +1,52 @@
+// --- Day 1: Sonar Sweep ---
+import * as fs from 'fs';
+
+export class SonarSweeper {
+    private lines: string[] = [];
+
+    public RunSilver(){
+        this.lines = this.Read();
+
+        let previous: number = 0;
+        let count: number = 0;
+
+        for (let index = 0; index < this.lines.length; ++index) {
+            const i = parseInt(this.lines[index]);
+            if (previous != 0 && i > previous){
+                ++count;
+            }
+
+            previous = i;
+        }
+
+        console.log(count);
+    }
+
+    public RunGold(){
+        this.lines = this.Read();
+
+        let sumPrevious: number = 0;
+        let count: number = 0;
+
+        for (let index = 0; index < this.lines.length - 2; ++index) {
+            const i1 = parseInt(this.lines[index]);
+            const i2 = parseInt(this.lines[index + 1]);
+            const i3 = parseInt(this.lines[index + 2]);
+
+            const sumCurrent = i1 + i2 + i3;
+            if (sumPrevious != 0 && sumPrevious < sumCurrent){
+                ++count;
+            }
+
+            sumPrevious = sumCurrent;
+        }
+
+        console.log(count);
+    }
+
+    
+    Read() : string[] {
+        const input = fs.readFileSync('day01/typescript/aspik79/input.txt','utf8');
+        return input.split('\r\n')
+    }    
+}
