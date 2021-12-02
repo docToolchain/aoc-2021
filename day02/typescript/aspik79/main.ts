08c3b4b5fde8813b7a88922c0d88419638a1159d
Christoph Jobmann <christoph.jobmann@deutschebahn.com>
2021-12-02 08:14:44 +0100

    Solution for day02 Minor fixes for day01

---
+++ day02/typescript/aspik79/main.ts
@@ -0,0 +1,14 @@
+import { Dive } from "./dive";
+
+console.log('day 2');
+
+// Dive solution by Christoph Jobmann
+const d = new Dive();
+
+console.log();
+console.log('Part 1:');
+d.RunSilver();
+
+console.log();
+console.log('Part 2:');
+d.RunGold();
