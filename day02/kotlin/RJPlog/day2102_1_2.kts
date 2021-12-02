be11ed6bb79bd7dd228f8facf75767ceecc23b18
RJPlog <tfrenz67@gmail.com>
2021-12-02 05:29:57 +0000

    Day02 Solution

--- day02/kotlin/RJPlog/day2102_1_2.kts
+++ day02/kotlin/RJPlog/day2102_1_2.kts
@@ -1,46 +1,50 @@
 import java.io.File
 
-// tag::sonar_sweep[]
-fun sonar_sweep(): Int {
+// tag::dive_1[]
+fun dive_1(): Int {
 	
-	var result: Int = 0
-	var samples = mutableListOf<Int>()
-	
-	File("day2102_puzzle_input.txt").forEachLine {
-		samples.add(it.toInt())
-	}
-
-	for (i in 1..samples.size-1) {
-		if (samples[i] > samples[i-1]){
-			result++
-		}
-	}
-	return result
-}
-// end::sonar_sweep[]
-
-// tag::sonar_sweep_noise[]
-fun sonar_sweep_noise(): Int {
-	
-	var result: Int = 0
-	var samples = mutableListOf<Int>()
+	var depth: Int = 0
+	var horizontal: Int = 0
 	
 	File("day2102_puzzle_input.txt").forEachLine {
-		samples.add(it.toInt())
+
+		var instruction = it.split(' ')
+		
+		if (instruction[0].equals("forward")) {
+			horizontal = horizontal + instruction[1].toInt()
+		} else if (instruction[0].equals("down")) {
+			depth = depth + instruction[1].toInt()
+		} else if (instruction[0].equals("up")) {
+			depth = depth - instruction[1].toInt()
+		}   
 	}
-
-	for (i in 1..samples.size-1-2) {
-
-		var prev_sum: Int = samples[i-1]+samples[i]+ samples[i+1]
-		var cur_sum: Int = samples[i]+samples[i+1]+ samples[i+2]
-
-		if (cur_sum > prev_sum){
-			result++
-		}
-	}
-	return result
+	return depth*horizontal
 }
-// end::sonar_sweep_noise[]
+// end::dive_1[]
+
+// tag::dive_2[]
+fun dive_2(): Int {
+	
+	var depth: Int = 0
+	var horizontal: Int = 0
+	var aim: Int = 0
+	
+	File("day2102_puzzle_input.txt").forEachLine {
+
+		var instruction = it.split(' ')
+		
+		if (instruction[0].equals("forward")) {
+			horizontal = horizontal + instruction[1].toInt()
+			depth = depth + aim * + instruction[1].toInt()
+		} else if (instruction[0].equals("down")) {
+			aim = aim + instruction[1].toInt()
+		} else if (instruction[0].equals("up")) {
+			aim = aim - instruction[1].toInt()
+		}   
+	}
+	return depth*horizontal
+}
+// end::dive_2[]
 
 
 //fun main(args: Array<String>) {
@@ -48,25 +52,25 @@ fun sonar_sweep_noise(): Int {
 	var solution2: Int
 
 // tag::part_1[]
-	solution1 = sonar_sweep()
+	solution1 = dive_1()
 // end::part_1[]
 
 // tag::part_2[]
-	solution2 = sonar_sweep_noise()
+	solution2 = dive_2()
 // end::part_2[]
 
 // tag::output[]
 // print solution for part 1
-	println("**************************")
-	println("--- Day 1: Sonar Sweep ---")
-	println("**************************")
+	println("*******************")
+	println("--- Day 2: Dive ---")
+	println("*******************")
 	println("Solution for part1")
-	println("   $solution1 are larger than the previous measurement ")
+	println("   $solution1 is your final horizontal position multiplied by final depth ")
 	println()
 // print solution for part 2
 	println("**************************")
 	println("Solution for part2")
-	println("   $solution2 sums are larger than the previous measurement")
+	println("   $solution2 is your final horizontal position multiplied by final depth")
 	println()
 // end::output[]
 //}
