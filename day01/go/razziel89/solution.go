bf1e01bd64c866c1d7c6a6cc7a80af99187b3cd7
Torsten Sachse <torsten.sachse@gmail.com>
2021-12-02 09:38:15 +0100

    Fix unreachable code

--- day01/go/razziel89/solution.go
+++ day01/go/razziel89/solution.go
@@ -22,11 +22,11 @@ func main() {
 		windowSize = windowFromEnv
 	}
 	depths, err := ReadLinesAsInts()
-	if len(depths) < windowSize {
-		log.Fatalf("cannot process fewer numbers than %d but got %d", windowSize, len(depths))
-	}
 	if err != nil {
 		log.Fatalf("cannot read depth values from stdin due to %v", err.Error())
+	}
+	if len(depths) < windowSize {
+		log.Fatalf("cannot process fewer numbers than %d but got %d", windowSize, len(depths))
 	}
 	depthsWithoutNoise := SlidingWindow(depths, windowSize, Sum)
 	increments := CountIncrements(depthsWithoutNoise)
