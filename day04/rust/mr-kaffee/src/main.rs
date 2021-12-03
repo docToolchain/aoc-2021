81c551be60490163242ba266c613396fa9b0e7bd
Peter Wieland <peter@die-wielands.net>
2021-12-03 09:24:10 +0100

    Solution day04 / rust / mr-kaffee
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

---
+++ day04/rust/mr-kaffee/src/main.rs
@@ -0,0 +1,20 @@
+use mr_kaffee_2021_04::*;
+use std::{fs, time::Instant};
+
+fn read_input() -> String {
+    fs::read_to_string("input.txt").expect("Could not read 'input.txt'")
+}
+
+fn main() {     
+    // solve part 1
+    let instant_1 = Instant::now();
+    let (mut boards, draws) = parse(&read_input());
+    let (sol_1, sol_2) = get_scores(&mut boards, &draws);
+    println!("Solved part 1 in {:?}: {:?}", instant_1.elapsed(), sol_1);
+    assert_eq!(10_680, sol_1);
+
+    // solve part 2
+    let instant_2 = Instant::now();
+    println!("Solved part 2 in {:?}: {:?}", instant_2.elapsed(), sol_2);
+    assert_eq!(31_892, sol_2);
+}
