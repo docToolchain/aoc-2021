5ed938d8ad29c6ea78ad37fdf843dbbcf1ca47cf
fallshare <fallscheer@googlemail.com>
2021-12-04 19:30:14 +0000

    Day 03 - Fallscheer - Star 2

--- day03/python/fallshare/solution.py
+++ day03/python/fallshare/solution.py
@@ -1,3 +1,4 @@
+import math
 
 input = list()
 
@@ -35,6 +36,69 @@ print(f"Gamma rate: {list_to_int(msb)}")
 print(f"Epsilon rate: {list_to_int(lsb)}")
 print(f"Power consumption: {list_to_int(msb) * list_to_int(lsb)}")
 print("---------------------------")
+print("Solution Star 2:")
+
+remaining_numbers = input.copy()
+for column in range(len(input[0])):
+    #determine msb of renaming numbers at position column
+    most_commonvalue = 0
+    least_commonvalue = 0
+
+    one_amount = 0
+    for row in range(len(remaining_numbers)):
+        if remaining_numbers[row][column] == 1:
+            one_amount += 1
+    if one_amount >= round(len(remaining_numbers) / 2):
+        most_commonvalue = 1
+        least_commonvalue = 0
+    else:
+        most_commonvalue = 0
+        least_commonvalue = 1
+     
+    #add numbers that match bit criteria to remaining numbers
+    matching_numbers = list()
+    for row in range(len(remaining_numbers)):
+        if remaining_numbers[row][column] == most_commonvalue:
+            matching_numbers.append(remaining_numbers[row])
+    
+    remaining_numbers = matching_numbers
+
+oxygen_gen_rate = list_to_int(remaining_numbers[0])
 
 
+
+
+remaining_numbers = input.copy()
+for column in range(len(input[0])):
+    #determine msb of renaming numbers at position column
+    most_commonvalue = 0
+    least_commonvalue = 0
+    one_amount = 0
+    for row in range(len(remaining_numbers)):
+        if remaining_numbers[row][column] == 1:
+            one_amount += 1
+    print(f"{one_amount} - {math.ceil(len(remaining_numbers) / 2)}")
+    if one_amount >= math.ceil(len(remaining_numbers) / 2):
+        most_commonvalue = 1
+        least_commonvalue = 0
+    else:
+        most_commonvalue = 0
+        least_commonvalue = 1
+    print(remaining_numbers)
+    print(least_commonvalue)
+    #add numbers that match bit criteria to remaining numbers
+    matching_numbers = list()
+    for row in range(len(remaining_numbers)):
+        if remaining_numbers[row][column] == least_commonvalue:
+            matching_numbers.append(remaining_numbers[row])
+    
+    remaining_numbers = matching_numbers
+
+    if len(remaining_numbers) == 1:
+        break
+scrubbing_rate = list_to_int(remaining_numbers[0])
+
+print(f"Oxygen generator rate: {oxygen_gen_rate}")
+print(f"CO2 scrubber rate: {scrubbing_rate}")
+print(f"Life support rate: {oxygen_gen_rate * scrubbing_rate}")
 
