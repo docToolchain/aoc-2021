654116135ea2c377a86553d4c352f8f7ba6cb1eb
Torsten Sachse <torsten.sachse@gmail.com>
2021-12-05 13:28:39 +0100

    Add initial grid and geonetrics version

--- day05/go/razziel89/grid.go
+++ day05/go/razziel89/grid.go
@@ -2,76 +2,173 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
+
+const (
+	vecSep         = ","
+	tokensPerPoint = 2
+	lineSep        = "->"
+	tokensPerLine  = 2
+)
+
+func trimStrings(sli []string) []string {
+	result := make([]string, 0, len(sli))
+	for _, val := range result {
+		result = append(result, strings.TrimSpace(val))
+	}
+	return result
+}
+
+func strSliceToIntSlice(sli []string) ([]int, error) {
+	// I wish Go had a map function...
+	result := make([]int, 0, len(sli))
+	for _, val := range sli {
+		conv, err := strconv.Atoi(val)
+		if err != nil {
+			return []int{}, err
+		}
+		result = append(result, conv)
+	}
+	return result, nil
+}
 
 // tag::set[]
 
-// CountingSet is a set that also knows how often each element has been added. It does support
-// non-empty strings only.
-type CountingSet map[string]int
+// Vec is a 2D vector.
+type Vec struct {
+	x, y int
+}
 
-// Add adds an entry to the set. The empty string is not supported!
-func (c *CountingSet) Add(entry string) error {
-	if len(entry) == 0 {
-		return fmt.Errorf("empty string not supported in counting set")
+// VecFromStr converts a sring into a vector.
+func VecFromStr(str string) (Vec, error) {
+	fields := trimStrings(strings.Split(str, vecSep))
+	if len(fields) != tokensPerPoint {
+		return Vec{}, fmt.Errorf("cannot parse %v as vector, wrong number of fields", str)
 	}
+	ints, err := strSliceToIntSlice(fields)
+	if err != nil {
+		return Vec{}, fmt.Errorf("cannot parse %v as vector, %v", str, err.Error)
+	}
+	result := Vec{
+		x: ints[0],
+		y: ints[1],
+	}
+	return result, nil
+}
+
+// Add adds one vector to another one.
+func (v Vec) Add(delta Vec) Vec {
+	result := Vec{
+		x: v.x + delta.x,
+		y: v.y + delta.y,
+	}
+	return result
+}
+
+// Mul multiplies each component of a vector with a number.
+func (v Vec) Mul(factor int) Vec {
+	result := Vec{
+		x: v.x * factor,
+		y: v.y * factor,
+	}
+	return result
+}
+
+// Inv inverts a vector.
+func (v Vec) Inv() Vec {
+	return v.Mul(-1)
+}
+
+// Sub subtracts a vector'v data from another one'v.
+func (v Vec) Sub(delta Vec) Vec {
+	return v.Add(delta.Inv())
+}
+
+// Normalize returns a unit vector with the same direction as the original vector. For now, this
+// does not support diagonals.
+func (v Vec) Normalize() (Vec, error) {
+	if v.y == 0 {
+		return Vec{x: 1}, nil
+	} else if v.x == 0 {
+		return Vec{y: 1}, nil
+	} else {
+		return Vec{}, fmt.Errorf("cannot normalize %v", v)
+	}
+}
+
+// Line is a line in 2D with a start and an end.
+type Line struct {
+	start, end Vec
+}
+
+// LineFromStr converts a sring into a line.
+func LineFromStr(str string) (Line, error) {
+	fields := trimStrings(strings.Split(str, lineSep))
+	if len(fields) != tokensPerLine {
+		return Line{}, fmt.Errorf("cannot parse %v as line, wrong number of fields", str)
+	}
+	start, err := VecFromStr(fields[0])
+	if err != nil {
+		return Line{}, fmt.Errorf("cannot parse %v as line, %v", str, err.Error())
+	}
+	end, err := VecFromStr(fields[1])
+	if err != nil {
+		return Line{}, fmt.Errorf("cannot parse %v as line, %v", str, err.Error())
+	}
+	result := Line{
+		start: start,
+		end:   end,
+	}
+	return result, nil
+}
+
+// Points determines all points on this line.
+func (l Line) Points() ([]Vec, error) {
+	result := []Vec{}
+	direction, err := l.end.Sub(l.start).Normalize()
+	if err != nil {
+		return []Vec{}, err
+	}
+	pos := l.start
+	for pos != l.end {
+		result = append(result, pos)
+		pos = pos.Add(direction)
+	}
+	result = append(result, pos)
+	return result, nil
+}
+
+// Grid is a lazily evaluated grid that supports marking points on it.
+type Grid map[Vec]int
+
+// Mark makrs a point on the grid once.
+func (g *Grid) Mark(entry Vec) {
 	// We don't have to handle non-existing values here since Go returns the zero value (0 for
 	// integers) for such entries.
-	(*c)[entry] = (*c)[entry] + 1
-	return nil
+	(*g)[entry] = (*g)[entry] + 1
 }
 
-// Count determines how often an entry has been added to the set.
-func (c *CountingSet) Count(entry string) int {
-	return (*c)[entry]
+// Count determines how often a point has been marked.
+func (g *Grid) Count(entry Vec) int {
+	return (*g)[entry]
 }
 
-// RemoveAll removes all counts for a specific key.
-func (c *CountingSet) RemoveAll(entry string) {
-	delete(*c, entry)
+// RemoveAll removes all markings for a specific point.
+func (g *Grid) RemoveAll(entry Vec) {
+	delete(*g, entry)
 }
 
-// MostCommon determines the most common entry in the set. If the set is empty, this returns the
-// empty string! A non-empty tie breaker will be returned in case there are multiple most common
-// entries. If the tie breaker is empty but there are duplicates, an empty string will be returned.
-func (c *CountingSet) MostCommon(tieBreaker string) string {
-	return c.filter(
-		tieBreaker,
-		func(i1, i2 int) bool {
-			return i1 > i2
-		},
-	)
-}
+// FilterFn is a type that can be used for FilterCounts to filter counts that fulfil a predicate.
+type FilterFn = func(int) bool
 
-// LeastCommon determines the least common entry in the set. If the set is empty, this returns the
-// empty string! A non-empty tie breaker will be returned in case there are multiple least common
-// entries. If the tie breaker is empty but there are duplicates, an empty string will be returned.
-func (c *CountingSet) LeastCommon(tieBreaker string) string {
-	return c.filter(
-		tieBreaker,
-		func(i1, i2 int) bool {
-			return i1 < i2
-		},
-	)
-}
-
-type comparisonFunc = func(int, int) bool
-
-func (c *CountingSet) filter(tieBreaker string, cmpFn comparisonFunc) string {
-	var result string
-	var resultCount int
-	foundOne := false
-	for entry, count := range *c {
-		if !foundOne || cmpFn(count, resultCount) {
-			foundOne = true
-			result = entry
-			resultCount = count
-		}
-	}
-	// Check whether there are any duplicate findings and handle appropriately.
-	for entry, count := range *c {
-		if count == resultCount && entry != result {
-			return tieBreaker
+// FilterCounts allow to filter counts using a FilterFn.
+func (g *Grid) FilterCounts(filterFn FilterFn) []Vec {
+	result := []Vec{}
+	for point, count := range *g {
+		if filterFn(count) {
+			result = append(result, point)
 		}
 	}
 	return result
