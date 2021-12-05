4697fbd8048ff0f1a6ebd6da5151838201e40965
Peter Wieland <peter@die-wielands.net>
2021-12-05 13:33:41 +0100

    Solution template day06 / rust / mr-kaffee
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

---
+++ day06/rust/mr-kaffee/src/lib.rs
@@ -0,0 +1,25 @@
+//tag::parse[]
+///
+/// ```
+/// # use mr_kaffee_2021_06::*;
+/// assert_eq!(vec![0, 1], parse("0\n1"));
+/// ```
+pub fn parse(content: &str) -> Vec<usize> {
+    content.lines().map(|line| line.parse().expect("Could not parse")).collect()
+}
+//end::parse[]
+
+//tag::tests[]
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const CONTENT: &str = "0\n1";
+    const DATA: &'static [usize] = &[0, 1];
+
+    #[test]
+    fn test_parse() {
+        assert_eq!(DATA, parse(CONTENT));
+    }
+}
+//end::tests[]
