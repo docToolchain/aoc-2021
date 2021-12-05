e86973d4cc4ee5614186d2433e40a36e09dd2c9a
Torsten Sachse <torsten.sachse@gmail.com>
2021-12-05 12:40:39 +0100

    Start from a mixture of day 3 and day 4's solutions

---
+++ day05/go/razziel89/solution.go
@@ -0,0 +1,15 @@
+// Package main is the main executable for razziel89's solution for this day's advent.
+package main
+
+import (
+	"fmt"
+)
+
+// tag::solution[]
+
+//nolint: funlen
+func main() {
+	fmt.Println("hello world")
+}
+
+// end::solution[]
