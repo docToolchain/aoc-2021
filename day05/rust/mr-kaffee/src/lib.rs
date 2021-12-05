93581497dfbdd97549b80828ed0539dc0835c5bc
Peter Wieland <peter@die-wielands.net>
2021-12-05 11:28:36 +0100

    Improved solution day05 / rust / mr-kaffee
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day05/rust/mr-kaffee/src/lib.rs
+++ day05/rust/mr-kaffee/src/lib.rs
@@ -1,10 +1,8 @@
-use std::cmp;
+use std::cmp::{self, Ordering};
 
 //tag::parse[]
-/// parse lines ``"x1,y1 -> x2,y2"`` to toples ``(x1, y1, x2, y2)``
-///
-/// the lines are normalized so that ``x1 <= x2`` and ``x1 == x2`` implies ``y1 <= y2``
-pub fn parse(content: &str) -> Vec<(usize, usize, usize, usize)> {
+/// parse lines ``"x1,y1 -> x2,y2"`` to tuples ``(x1, y1, x2, y2)``
+pub fn parse(content: &str) -> Vec<(isize, isize, isize, isize)> {
     content
         .lines()
         .map(|line| {
@@ -32,13 +30,7 @@ pub fn parse(content: &str) -> Vec<(usize, usize, usize, usize)> {
                 .parse()
                 .expect("Could not parse y2");
 
-            if x2 < x1 || (x1 == x2) && (y2 < y1) {
-                // reverse order
-                // ensure that x1 <= x2 and x1 == x2 implies y1 <= y2
-                (x2, y2, x1, y1)
-            } else {
-                (x1, y1, x2, y2)
-            }
+            (x1, y1, x2, y2)
         })
         .collect()
 }
@@ -49,14 +41,14 @@ pub fn parse(content: &str) -> Vec<(usize, usize, usize, usize)> {
 ///
 /// returns ``(x_min, y_min, x_max, y_max)`` so that all points ``(x, y)`` on
 /// any line satisfy ``x_min <= x < x_max`` and ``y_min <= y < y_max``
-pub fn get_bbox(lines: &[(usize, usize, usize, usize)]) -> (usize, usize, usize, usize) {
+pub fn get_bbox(lines: &[(isize, isize, isize, isize)]) -> (isize, isize, isize, isize) {
     lines.iter().fold(
-        (usize::MAX, usize::MAX, usize::MIN, usize::MIN),
+        (isize::MAX, isize::MAX, isize::MIN, isize::MIN),
         |(x_min, y_min, x_max, y_max), (x1, y1, x2, y2)| {
             (
-                cmp::min(x_min, *x1),
+                cmp::min(cmp::min(x_min, *x1), *x2),
                 cmp::min(cmp::min(y_min, *y1), *y2),
-                cmp::max(x_max, *x2 + 1),
+                cmp::max(cmp::max(x_max, *x1 + 1), *x2 + 1),
                 cmp::max(cmp::max(y_max, *y1 + 1), *y2 + 1),
             )
         },
@@ -65,38 +57,27 @@ pub fn get_bbox(lines: &[(usize, usize, usize, usize)]) -> (usize, usize, usize,
 //end::bbox[]
 
 //tag::count_overlaps[]
-pub fn count_overlaps(lines: &[(usize, usize, usize, usize)], incl_diagonal: bool) -> usize {
+pub fn count_overlaps(lines: &[(isize, isize, isize, isize)], incl_diagonal: bool) -> usize {
     let (x_min, y_min, x_max, y_max) = get_bbox(lines);
+
     let width = x_max - x_min;
-    let mut counts = vec![0usize; width * (y_max - y_min)];
+    let mut counts = vec![0usize; (width * (y_max - y_min)) as usize];
 
     for (x1, y1, x2, y2) in lines {
-        if x1 == x2 {
-            // vertical line
-            for y in *y1..=*y2 {
-                counts[*x1 - x_min + width * (y - y_min)] += 1;
-            }
-        } else if y1 == y2 {
-            // horizontal line
-            for x in *x1..=*x2 {
-                counts[x - x_min + width * (*y1 - y_min)] += 1;
-            }
-        } else if incl_diagonal {
-            // check line is actually diagonal
-            if x2 - x1 != cmp::max(y1, y2) - cmp::min(y1, y2) {
-                panic!("Not a diagnoal line: {},{} -> {},{}", x1, y1, x2, y2);
-            }
-
-            if y2 < y1 {
-                // bottom left to top right (x and y progress in different directions)
-                for k in 0..x2 - x1 + 1 {
-                    counts[x1 + k - x_min + width * (y1 - k - y_min)] += 1;
-                }
-            } else {
-                // top left to bottom right
-                for k in 0..x2 - x1 + 1 {
-                    counts[x1 + k - x_min + width * (y1 + k - y_min)] += 1;
-                }
+        if incl_diagonal || x1 == x2 || y1 == y2 {
+            let dx = match x1.cmp(&x2) {
+                Ordering::Equal => 0,
+                Ordering::Greater => -1,
+                Ordering::Less => 1,
+            };
+            let dy = match y1.cmp(&y2) {
+                Ordering::Equal => 0,
+                Ordering::Greater => -1,
+                Ordering::Less => 1,
+            };
+            let len = cmp::max((x2 - x1) * dx, (y2 - y1) * dy) + 1;
+            for k in 0..len {
+                counts[(x1 + k * dx - x_min + width * (y1 + k * dy - y_min)) as usize] += 1;
             }
         }
     }
@@ -121,15 +102,15 @@ mod tests {
 0,0 -> 8,8
 5,5 -> 8,2";
 
-    const LINES: &'static [(usize, usize, usize, usize)] = &[
+    const LINES: &'static [(isize, isize, isize, isize)] = &[
         (0, 9, 5, 9),
-        (0, 8, 8, 0),
-        (3, 4, 9, 4),
-        (2, 1, 2, 2),
+        (8, 0, 0, 8),
+        (9, 4, 3, 4),
+        (2, 2, 2, 1),
         (7, 0, 7, 4),
-        (2, 0, 6, 4),
+        (6, 4, 2, 0),
         (0, 9, 2, 9),
-        (1, 4, 3, 4),
+        (3, 4, 1, 4),
         (0, 0, 8, 8),
         (5, 5, 8, 2),
     ];
