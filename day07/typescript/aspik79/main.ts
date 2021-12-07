8a7f7dcc8a80c25a7491bf9b0ae5e2910db6ca96
Christoph Jobmann <christoph.jobmann@deutschebahn.com>
2021-12-07 07:01:58 +0100

    day 07

---
+++ day07/typescript/aspik79/main.ts
@@ -0,0 +1,7 @@
+// Day 07: The Treachery of Whales
+// Solution by Christoph Jobmann
+
+import { CrabAligner } from "./crabAligner";
+
+console.log(new CrabAligner(true).countMinAlignmentFuel());
+console.log(new CrabAligner(false).countMinAlignmentFuel());
