2bb1c55d2916035e40c84dfc00385a8f5a83e3b8
Peter Wieland <peter@die-wielands.net>
2021-12-10 07:12:33 +0100

    Solution day10 / rust / mr-kaffee
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day10/rust/mr-kaffee/src/lib.rs
+++ day10/rust/mr-kaffee/src/lib.rs
@@ -1,23 +1,113 @@
+use std::collections::VecDeque;
+
 // tag::parse[]
-///
-/// ```
-/// # use mr_kaffee_2021_10::*;
-/// assert_eq!(vec![0, 1], parse("0\n1"));
-/// ```
-pub fn parse(content: &str) -> Vec<usize> {
-    content.lines().map(|line| line.parse().expect("Could not parse")).collect()
+pub fn parse(content: &str) -> Vec<String> {
+    content.lines().map(|s| s.to_string()).collect::<Vec<_>>()
 }
 // end::parse[]
 
 // tag::part1[]
-pub fn solution_1(data: &[usize]) -> usize {
-    data[0]
+/// return illegal char's score if any in as ``Some`` value, otherwise ``None``
+pub fn find_illegal_char(line: &str) -> Option<usize> {
+    let mut queue = VecDeque::new();
+    for c in line.chars() {
+        match c {
+            '(' => {
+                queue.push_back(')');
+            }
+            '[' => {
+                queue.push_back(']');
+            }
+            '{' => {
+                queue.push_back('}');
+            }
+            '<' => {
+                queue.push_back('>');
+            }
+            ')' => {
+                if c != queue.pop_back().expect("Empty queue") {
+                    return Some(3);
+                }
+            }
+            ']' => {
+                if c != queue.pop_back().expect("Empty queue") {
+                    return Some(57);
+                }
+            }
+            '}' => {
+                if c != queue.pop_back().expect("Empty queue") {
+                    return Some(1197);
+                }
+            }
+            '>' => {
+                if c != queue.pop_back().expect("Empty queue") {
+                    return Some(25137);
+                }
+            }
+            _ => panic!("Illegal charachter '{}'", c),
+        }
+    }
+    None
+}
+
+/// Calculate some of scores of illegal chars
+pub fn solution_1(lines: &[String]) -> usize {
+    lines
+        .iter()
+        .filter_map(|line| find_illegal_char(line))
+        .sum()
 }
 // end::part1[]
 
 // tag::part2[]
-pub fn solution_2(data: &[usize]) -> usize {
-    data[1]
+/// get the repair score if the line is incomplete as a ``Some`` value, otherwise return ``None``
+pub fn get_repair_score(line: &str) -> Option<usize> {
+    let mut queue = VecDeque::new();
+    for c in line.chars() {
+        match c {
+            '(' => {
+                queue.push_front(')');
+            }
+            '[' => {
+                queue.push_front(']');
+            }
+            '{' => {
+                queue.push_front('}');
+            }
+            '<' => {
+                queue.push_front('>');
+            }
+            ')' | '>' | '}' | ']' => {
+                if c != queue.pop_front().expect("Empty queue") {
+                    return None;
+                }
+            }
+            _ => panic!("Illegal charachter '{}'", c),
+        }
+    }
+
+    Some(
+        queue
+            .iter()
+            .map(|c| match c {
+                ')' => 1,
+                ']' => 2,
+                '}' => 3,
+                '>' => 4,
+                _ => panic!("Illegal charachter '{}'", c),
+            })
+            .fold(0, |score, v| score * 5 + v),
+    )
+}
+
+/// get the middle repair score
+pub fn solution_2(lines: &[String]) -> usize {
+    let mut scores = lines
+        .iter()
+        .filter_map(|line| get_repair_score(line))
+        .collect::<Vec<_>>();
+    scores.sort();
+    scores[scores.len() / 2]
 }
 // end::part2[]
 
@@ -26,12 +116,73 @@ pub fn solution_2(data: &[usize]) -> usize {
 mod tests {
     use super::*;
 
-    const CONTENT: &str = "0\n1";
-    const DATA: &'static [usize] = &[0, 1];
+    const CONTENT: &str = "[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]";
 
     #[test]
-    fn test_parse() {
-        assert_eq!(DATA, parse(CONTENT));
+    fn test_find_illegal_char() {
+        let lines = parse(CONTENT);
+        assert_eq!(
+            vec![
+                None,
+                None,
+                Some(1197),
+                None,
+                Some(3),
+                Some(57),
+                None,
+                Some(3),
+                Some(25137),
+                None
+            ],
+            lines
+                .iter()
+                .map(|line| find_illegal_char(line))
+                .collect::<Vec<_>>()
+        );
+    }
+
+    #[test]
+    fn test_solution_1() {
+        let lines = parse(CONTENT);
+        assert_eq!(26_397, solution_1(&lines));
+    }
+
+    #[test]
+    fn test_get_repair_score() {
+        let lines = parse(CONTENT);
+        assert_eq!(
+            vec![
+                Some(288957),
+                Some(5566),
+                None,
+                Some(1480781),
+                None,
+                None,
+                Some(995444),
+                None,
+                None,
+                Some(294)
+            ],
+            lines
+                .iter()
+                .map(|line| get_repair_score(line))
+                .collect::<Vec<_>>()
+        );
+    }
+
+    #[test]
+    fn test_solution_2() {
+        let lines = parse(CONTENT);
+        assert_eq!(288_957, solution_2(&lines));
     }
 }
 // end::tests[]
--- day10/rust/mr-kaffee/src/main.rs
+++ day10/rust/mr-kaffee/src/main.rs
@@ -7,11 +7,11 @@ fn main() {
     let data = parse(include_str!("../input.txt"));
     let sol_1 = solution_1(&data);
     println!("Solved part 1 in {:?}: {:?}", instant_1.elapsed(), sol_1);
-    assert_eq!(0, sol_1);
+    assert_eq!(387_363, sol_1);
 
     // solve part 2
     let instant_2 = Instant::now();
     let sol_2 = solution_2(&data);
     println!("Solved part 2 in {:?}: {:?}", instant_2.elapsed(), sol_2);
-    assert_eq!(1, sol_2);
+    assert_eq!(4_330_777_059, sol_2);
 }
