61b0f85d19536ed49a7543dd437a6e399d2d52db
Peter Wieland <peter@die-wielands.net>
2021-12-11 08:11:09 +0100

    Test for parse day11
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day11/rust/mr-kaffee/src/lib.rs
+++ day11/rust/mr-kaffee/src/lib.rs
@@ -1,6 +1,6 @@
 // tag::parse[]
 /// parse energy levels
-/// 
+///
 /// # Panics
 /// if number of energy levels is not 10 x 10 = 100
 pub fn parse(content: &str) -> Vec<usize> {
@@ -113,6 +113,13 @@ mod tests {
 4846848554
 5283751526";
 
+    const ENERGIES: &'static [usize] = &[
+        5, 4, 8, 3, 1, 4, 3, 2, 2, 3, 2, 7, 4, 5, 8, 5, 4, 7, 1, 1, 5, 2, 6, 4, 5, 5, 6, 1, 7, 3,
+        6, 1, 4, 1, 3, 3, 6, 1, 4, 6, 6, 3, 5, 7, 3, 8, 5, 4, 7, 8, 4, 1, 6, 7, 5, 2, 4, 6, 4, 5,
+        2, 1, 7, 6, 8, 4, 1, 7, 2, 1, 6, 8, 8, 2, 8, 8, 1, 1, 3, 4, 4, 8, 4, 6, 8, 4, 8, 5, 5, 4,
+        5, 2, 8, 3, 7, 5, 1, 5, 2, 6,
+    ];
+
     const CONTENT_1: &str = "6594254334
 3856965822
 6375667284
@@ -148,7 +155,7 @@ mod tests {
 
     #[test]
     fn test_parse() {
-        assert_eq!(100, parse(CONTENT).len());
+        assert_eq!(ENERGIES, parse(CONTENT));
     }
 
     #[test]
