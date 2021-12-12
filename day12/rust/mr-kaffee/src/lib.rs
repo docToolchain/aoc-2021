6f6f37bb3c859403758fec898face05bd4e20b02
Peter Wieland <peter@die-wielands.net>
2021-12-12 18:22:24 +0100

    use direct addressing instead of maps
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day12/rust/mr-kaffee/src/lib.rs
+++ day12/rust/mr-kaffee/src/lib.rs
@@ -1,23 +1,65 @@
-use std::collections::{BTreeMap, BTreeSet, VecDeque};
+use std::collections::VecDeque;
 
-// tag::parse[]
-/// Parse the input in a map from nodes to sets of neighbor nodes
-pub fn parse<'a>(input: &'a str) -> BTreeMap<&'a str, BTreeSet<&'a str>> {
-    let mut map = BTreeMap::new();
-
-    for line in input.lines() {
-        let mut parts = line.split('-');
-
-        let lhs = parts.next().expect("No LHS");
-        let rhs = parts.next().expect("No RHS");
-        assert!(parts.next().is_none(), "More than two parts");
-
-        map.entry(lhs).or_insert(BTreeSet::new()).insert(rhs);
-        map.entry(rhs).or_insert(BTreeSet::new()).insert(lhs);
-    }
-    map
+/// map of caves
+pub struct CaveMap<'a> {
+    pub caves: Vec<&'a str>,
+    pub adjacents: Vec<Vec<usize>>,
+    pub start: usize,
+    pub end: usize,
 }
-// end::parse[]
+
+impl<'a> CaveMap<'a> {
+    // tag::parse[]
+    /// parse input to map
+    pub fn parse(input: &'a str) -> Self {
+        let mut caves = Vec::new();
+        let mut adjacents = Vec::new();
+        let mut start = 0;
+        let mut end = 0;
+
+        for line in input.lines() {
+            let mut parts = line.split('-').map(|part| {
+                caves
+                    .iter()
+                    .position(|cave| *cave == part)
+                    .unwrap_or_else(|| {
+                        caves.push(part);
+                        adjacents.push(Vec::new());
+                        // keep ID to start or end
+                        match part {
+                            "start" => start = caves.len() - 1,
+                            "end" => end = caves.len() - 1,
+                            _ => {}
+                        }
+                        caves.len() - 1
+                    })
+            });
+
+            let lhs = parts.next().expect("No LHS");
+            let rhs = parts.next().expect("No RHS");
+            assert!(parts.next().is_none(), "More than two parts");
+            adjacents[lhs].push(rhs);
+            adjacents[rhs].push(lhs);
+        }
+
+        CaveMap {
+            caves,
+            adjacents,
+            start,
+            end,
+        }
+    }
+    // end::parse[]
+
+    /// check whether cave with given id is small
+    pub fn is_small(&self, id: usize) -> bool {
+        self.caves[id].chars().next().unwrap().is_ascii_lowercase()
+    }
+}
+
+pub fn parse(input: &'static str) -> CaveMap {
+    CaveMap::parse(input)
+}
 
 // tag::get_path_count[]
 /// count number of distinct pathes using map
@@ -25,30 +67,25 @@ pub fn parse<'a>(input: &'a str) -> BTreeMap<&'a str, BTreeSet<&'a str>> {
 /// the flag ``no_duplicate_small`` controls whether small caves may be visited more than
 /// once. If the flag is set to ``true``, this is not allowed at all. If it is set to false,
 /// it is allowed at most once.
-pub fn get_path_count(map: &BTreeMap<&str, BTreeSet<&str>>, no_duplicate_small: bool) -> usize {
+pub fn get_path_count(map: &CaveMap, no_duplicate_small: bool) -> usize {
     let mut path_count = 0;
 
     let mut queue = VecDeque::new();
-    queue.push_back((vec!["start"], no_duplicate_small));
+    queue.push_back((vec![map.start], no_duplicate_small));
 
     while let Some((path, no_duplicate_small)) = queue.pop_front() {
         let cave = path.last().expect("Incomplete path is empty");
-        for adjacent in map.get(cave).expect("Node missing in map") {
-            match *adjacent {
-                "start" => continue, // never go back to "start"
-                "end" => { // new path to "end" found
-                    path_count += 1;
-                    continue;
-                }
-                _ => {} // no special treatment
+        for &adjacent in &map.adjacents[*cave] {
+            if adjacent == map.start {
+                // never go back to start
+                continue;
+            } else if adjacent == map.end {
+                // new path to "end" found
+                path_count += 1;
+                continue;
             }
 
-            let duplicate_small = adjacent
-                .chars()
-                .next()
-                .expect("Empty node")
-                .is_ascii_lowercase()
-                && path.contains(adjacent);
+            let duplicate_small = map.is_small(adjacent) && path.contains(&adjacent);
             if no_duplicate_small && duplicate_small {
                 // skip lower case already on path, if no duplicate small caves are allowed
                 continue;
@@ -68,13 +105,13 @@ pub fn get_path_count(map: &BTreeMap<&str, BTreeSet<&str>>, no_duplicate_small: 
 // end::get_path_count[]
 
 // tag::part1[]
-pub fn solution_1(map: &BTreeMap<&str, BTreeSet<&str>>) -> usize {
+pub fn solution_1(map: &CaveMap) -> usize {
     get_path_count(map, true)
 }
 // end::part1[]
 
 // tag::part2[]
-pub fn solution_2(map: &BTreeMap<&str, BTreeSet<&str>>) -> usize {
+pub fn solution_2(map: &CaveMap) -> usize {
     get_path_count(map, false)
 }
 // end::part2[]
@@ -82,6 +119,8 @@ pub fn solution_2(map: &BTreeMap<&str, BTreeSet<&str>>) -> usize {
 // tag::tests[]
 #[cfg(test)]
 mod tests {
+    use std::collections::{BTreeMap, BTreeSet};
+
     use super::*;
 
     const TEST_INPUT: &str = "start-A
@@ -92,7 +131,7 @@ b-d
 A-end
 b-end";
 
-    fn test_data() -> BTreeMap<&'static str, BTreeSet<&'static str>> {
+    fn get_test_data() -> BTreeMap<&'static str, BTreeSet<&'static str>> {
         BTreeMap::from([
             ("start", BTreeSet::from(["A", "b"])),
             ("A", BTreeSet::from(["start", "c", "b", "end"])),
@@ -103,19 +142,32 @@ b-end";
         ])
     }
 
+    fn get_adjacents<'a>(map: &'a CaveMap, name: &str) -> Option<impl Iterator<Item = &'a str>> {
+        map.caves
+            .iter()
+            .position(|cave| *cave == name)
+            .map(|idx| map.adjacents[idx].iter().map(|adj| map.caves[*adj]))
+    }
+
     #[test]
     fn test_parse() {
-        assert_eq!(test_data(), parse(TEST_INPUT));
+        let map = parse(TEST_INPUT);
+        for (name, adjacents) in get_test_data() {
+            assert_eq!(
+                Some(adjacents),
+                get_adjacents(&map, name).map(|it| it.collect())
+            );
+        }
     }
 
     #[test]
     fn test_solution_1() {
-        assert_eq!(10, solution_1(&test_data()))
+        assert_eq!(10, solution_1(&parse(&TEST_INPUT)))
     }
 
     #[test]
     fn test_solution_2() {
-        assert_eq!(36, solution_2(&test_data()))
+        assert_eq!(36, solution_2(&parse(&TEST_INPUT)))
     }
 }
 // end::tests[]
