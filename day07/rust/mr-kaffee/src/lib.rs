76dec9c3040c781b4b0825c4427a9b46b840b0a7
Peter Wieland <peter@die-wielands.net>
2021-12-13 22:54:03 +0100

    Direct solution for day07 based on median / mean
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day07/rust/mr-kaffee/src/lib.rs
+++ day07/rust/mr-kaffee/src/lib.rs
@@ -1,4 +1,4 @@
-//tag::parse[]
+// tag::parse[]
 pub fn parse(content: &str) -> Vec<usize> {
     content
         .trim()
@@ -6,9 +6,60 @@ pub fn parse(content: &str) -> Vec<usize> {
         .map(|v| v.parse().expect("Could not parse"))
         .collect()
 }
-//end::parse[]
+// end::parse[]
 
-//tag::get_fuel[]
+// tag::select[]
+/// Recursive calculation of k-th element.
+/// 
+/// # Examples
+/// 
+/// ```
+/// # use mr_kaffee_2021_07::*;
+/// let data = vec![1,4,7,12,8,13,101];
+/// assert_eq!(8, select(&data, data.len() / 2));
+/// ```
+pub fn select(data: &[usize], k: usize) -> usize {
+    if k >= data.len() {
+        panic!(
+            "Cannot get select rank {} of list of size {}",
+            k,
+            data.len()
+        );
+    }
+
+    if data.len() == 1 {
+        return data[0];
+    }
+
+    let pivot = data[0];
+
+    let lows = data
+        .into_iter()
+        .filter(|d| d < &&pivot)
+        .map(|d| *d)
+        .collect::<Vec<_>>();
+    let pivots = data
+        .into_iter()
+        .filter(|d| d == &&pivot)
+        .map(|d| *d)
+        .collect::<Vec<_>>();
+    let highs = data
+        .into_iter()
+        .filter(|d| d > &&pivot)
+        .map(|d| *d)
+        .collect::<Vec<_>>();
+
+    return if k < lows.len() {
+        select(&lows, k)
+    } else if k < lows.len() + pivots.len() {
+        pivots[0]
+    } else {
+        select(&highs, k - lows.len() - pivots.len())
+    };
+}
+// end::select[]
+
+// tag::get_fuel[]
 pub fn get_fuel(crabs: &[usize], position: usize, cost: fn(usize) -> usize) -> usize {
     crabs
         .iter()
@@ -21,14 +72,18 @@ pub fn get_fuel(crabs: &[usize], position: usize, cost: fn(usize) -> usize) -> u
         })
         .sum()
 }
-//end::get_fuel[]
+// end::get_fuel[]
 
-//tag::cost_functions[]
+// tag::cost_functions[]
+/// cost function for part 1: identity
 pub const COST_LINEAR: fn(usize) -> usize = |distance| distance;
-pub const COST_INCREASING: fn(usize) -> usize = |distance| distance * (distance + 1) / 2;
-//end::cost_functions[]
 
-//tag::optimize[]
+/// cost function for part 2: Euler formula for the sum 1 + ... + distance
+pub const COST_INCREASING: fn(usize) -> usize = |distance| distance * (distance + 1) / 2;
+// end::cost_functions[]
+
+// tag::optimize[]
+/// linear search optimization
 pub fn get_optimal_positions_fuel(crabs: &[usize], cost: fn(usize) -> usize) -> usize {
     let pos_min = *crabs.iter().min().expect("No min");
     let pos_max = *crabs.iter().max().expect("No max");
@@ -37,9 +92,69 @@ pub fn get_optimal_positions_fuel(crabs: &[usize], cost: fn(usize) -> usize) -> 
         .min()
         .expect("No min")
 }
-//end::optimize[]
+// end::optimize[]
 
-//tag::tests[]
+// tag::solutions[]
+/// solution for part 1
+/// 
+/// default: linear search, see [get_optimal_positions_fuel] and [COST_LINEAR]
+/// 
+/// if feature ``direct_solution`` is selected, calculate median as solution, see [select]
+pub fn solution_1(crabs: &[usize]) -> usize {
+    if cfg!(feature = "direct_solution") {
+        // calculate median and comput cost for median
+        get_fuel(crabs, select(&crabs, crabs.len() / 2), COST_LINEAR)
+    } else {
+        // linear search
+        get_optimal_positions_fuel(crabs, COST_LINEAR)
+    }
+}
+
+/// solution for part 1
+/// 
+/// default: linear search, see [get_optimal_positions_fuel] and [COST_INCREASING]
+/// 
+/// if feature ``direct_solution`` is selected, calculate mean and search for solution in neighborhood
+pub fn solution_2(crabs: &[usize]) -> usize {
+    if cfg!(feature = "direct_solution") {
+        // calculate mean and cost for mean
+        let mean = (crabs.iter().sum::<usize>() + crabs.len() - 1) / crabs.len();
+        let mut cost = get_fuel(crabs, mean, COST_INCREASING);
+
+        // search for improvements for increasing positions
+        let mut delta = 1;
+        loop {
+            let cost_upd = get_fuel(crabs, mean + delta, COST_INCREASING);
+            if cost_upd >= cost {
+                break;
+            }
+
+            cost = cost_upd;
+            delta += 1;
+        }
+
+        // if no improvment found, search for improvements for decreasing positions
+        if delta == 1 {
+            loop {
+                let cost_upd = get_fuel(crabs, mean - delta, COST_INCREASING);
+                if cost_upd >= cost {
+                    break;
+                }
+
+                cost = cost_upd;
+                delta += 1;
+            }
+        }
+
+        cost
+    } else {
+        // linear search
+        get_optimal_positions_fuel(crabs, COST_INCREASING)
+    }
+}
+// end::solutions[]
+
+// tag::tests[]
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -60,13 +175,13 @@ mod tests {
     }
 
     #[test]
-    fn test_get_optimal_positions_fuel() {
-        assert_eq!(37, get_optimal_positions_fuel(DATA, COST_LINEAR));
+    fn test_solution_1() {
+        assert_eq!(37, solution_1(DATA));
     }
 
     #[test]
-    fn test_get_optimal_positions_fuel_2() {
-        assert_eq!(168, get_optimal_positions_fuel(DATA, COST_INCREASING));
+    fn test_solution_2() {
+        assert_eq!(168, solution_2(DATA));
     }
 }
-//end::tests[]
+// end::tests[]
