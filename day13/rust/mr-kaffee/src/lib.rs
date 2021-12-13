fbb75421ef451f5e3f6df90cd83f18b12d54f327
Peter Wieland <peter@die-wielands.net>
2021-12-13 07:28:48 +0100

    Solution day13 / rust / mr-kaffee
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day13/rust/mr-kaffee/src/lib.rs
+++ day13/rust/mr-kaffee/src/lib.rs
@@ -1,23 +1,114 @@
+use std::{cmp, collections::BTreeSet, ops::Add};
+
 // tag::parse[]
-///
-/// ```
-/// # use mr_kaffee_2021_13::*;
-/// assert_eq!(vec![0, 1], parse("0\n1"));
-/// ```
-pub fn parse(content: &str) -> Vec<usize> {
-    content.lines().map(|line| line.parse().expect("Could not parse")).collect()
+pub fn parse(content: &str) -> (BTreeSet<(usize, usize)>, Vec<(bool, usize)>) {
+    let mut parts = content.split("\n\n");
+    let points = parts.next().expect("No points");
+    let folds = parts.next().expect("No folds");
+    assert!(parts.next().is_none(), "Excess data");
+
+    let points = points
+        .lines()
+        .map(|line| {
+            let mut parts = line.split(",");
+            (
+                parts
+                    .next()
+                    .expect("No x coordinate")
+                    .parse()
+                    .expect("Could not parse x"),
+                parts
+                    .next()
+                    .expect("No y coordinate")
+                    .parse()
+                    .expect("Could not parse y"),
+            )
+        })
+        .collect();
+
+    let folds = folds
+        .lines()
+        .map(|line| {
+            let mut parts = line.split("=");
+            (
+                match parts.next() {
+                    Some("fold along y") => true,
+                    Some("fold along x") => false,
+                    _ => panic!("Unexpected fold instruction"),
+                },
+                parts
+                    .next()
+                    .expect("No fold coordinate")
+                    .parse()
+                    .expect("Could not parse fold coordinate"),
+            )
+        })
+        .collect();
+
+    (points, folds)
 }
 // end::parse[]
 
+// tag::fold[]
+/// perform a single fold
+pub fn fold(
+    points: &BTreeSet<(usize, usize)>,
+    horizontal: bool,
+    fold_coordinate: usize,
+) -> BTreeSet<(usize, usize)> {
+    points
+        .iter()
+        .map(|(x, y)| {
+            if horizontal && y > &fold_coordinate {
+                (*x, fold_coordinate - (*y - fold_coordinate))
+            } else if !horizontal && x > &fold_coordinate {
+                (fold_coordinate - (*x - fold_coordinate), *y)
+            } else {
+                (*x, *y)
+            }
+        })
+        .collect()
+}
+// end::fold[]
+
 // tag::part1[]
-pub fn solution_1(data: &[usize]) -> usize {
-    data[0]
+/// count points after first fold operation
+pub fn solution_1(points: &BTreeSet<(usize, usize)>, folds: &[(bool, usize)]) -> usize {
+    let (horizontal, fold_coordinate) = folds.first().expect("No folds");
+    fold(points, *horizontal, *fold_coordinate).len()
 }
 // end::part1[]
 
 // tag::part2[]
-pub fn solution_2(data: &[usize]) -> usize {
-    data[1]
+/// perform all fold operations and return result as a ``String``
+pub fn solution_2(points: &BTreeSet<(usize, usize)>, folds: &[(bool, usize)]) -> String {
+    let mut points = points.to_owned();
+    for (horizontal, fold_coordinate) in folds {
+        points = fold(&points, *horizontal, *fold_coordinate);
+    }
+
+    // calculate bounding box
+    let (x_min, y_min, x_max, y_max) = points.iter().fold(
+        (usize::MAX, usize::MAX, 0, 0),
+        |(x_min, y_min, x_max, y_max), (x, y)| {
+            (
+                cmp::min(x_min, *x),
+                cmp::min(y_min, *y),
+                cmp::max(x_max, *x + 1),
+                cmp::max(y_max, *y + 1),
+            )
+        },
+    );
+
+    // assemble result string
+    (y_min..y_max)
+        .map(|y| {
+            (x_min..x_max)
+                .map(|x| if points.contains(&(x, y)) { '#' } else { ' ' })
+                .collect::<String>()
+        })
+        .map(|line| line.add("\n"))
+        .collect::<String>()
 }
 // end::part2[]
 
@@ -26,12 +117,74 @@ pub fn solution_2(data: &[usize]) -> usize {
 mod tests {
     use super::*;
 
-    const CONTENT: &str = "0\n1";
-    const DATA: &'static [usize] = &[0, 1];
+    const TEST_CONTENT: &str = "6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5";
+
+    const TEST_POINTS: [(usize, usize); 18] = [
+        (6, 10),
+        (0, 14),
+        (9, 10),
+        (0, 3),
+        (10, 4),
+        (4, 11),
+        (6, 0),
+        (6, 12),
+        (4, 1),
+        (0, 13),
+        (10, 12),
+        (3, 4),
+        (3, 0),
+        (8, 4),
+        (1, 10),
+        (2, 14),
+        (8, 10),
+        (9, 0),
+    ];
+
+    const TEST_FOLDS: &[(bool, usize)] = &[(true, 7), (false, 5)];
+
+    const TEST_RESULT: &str = "#####\n\
+                               #   #\n\
+                               #   #\n\
+                               #   #\n\
+                               #####\n";
 
     #[test]
     fn test_parse() {
-        assert_eq!(DATA, parse(CONTENT));
+        let (points, folds) = parse(&TEST_CONTENT);
+        assert_eq!(BTreeSet::from(TEST_POINTS), points);
+        assert_eq!(TEST_FOLDS, folds);
+    }
+
+    #[test]
+    fn test_solution_1() {
+        let (points, folds) = parse(&TEST_CONTENT);
+        assert_eq!(17, solution_1(&points, &folds));
+    }
+
+    #[test]
+    fn test_solution_2() {
+        let (points, folds) = parse(&TEST_CONTENT);
+        assert_eq!(TEST_RESULT, solution_2(&points, &folds));
     }
 }
 // end::tests[]
--- day13/rust/mr-kaffee/src/main.rs
+++ day13/rust/mr-kaffee/src/main.rs
@@ -4,24 +4,28 @@ use std::time::Instant;
 /// puzzle input
 pub const INPUT: &str = include_str!("../input.txt");
 /// expected result part 1
-pub const EXP_1: usize = 0;
-/// expected result part 2
-pub const EXP_2: usize = 1;
+pub const EXP_1: usize = 675;
+pub const EXP_2: &str = "#  # #### #  # #  # #### ####   ## ####\n\
+                         #  #    # # #  #  # #    #       #    #\n\
+                         ####   #  ##   #### ###  ###     #   # \n\
+                         #  #  #   # #  #  # #    #       #  #  \n\
+                         #  # #    # #  #  # #    #    #  # #   \n\
+                         #  # #### #  # #  # #    ####  ##  ####\n";
 
-fn main() {     
+fn main() {
     let start = Instant::now();
-    let data = parse(INPUT);
+    let (points, folds) = parse(INPUT);
 
     // solve part 1
     let start_1 = Instant::now();
-    let sol_1 = solution_1(&data);
+    let sol_1 = solution_1(&points, &folds);
     println!("Solved part 1 in {:?}: {:?}", start_1.elapsed(), sol_1);
     assert_eq!(EXP_1, sol_1);
 
     // solve part 2
     let start_2 = Instant::now();
-    let sol_2 = solution_2(&data);
-    println!("Solved part 2 in {:?}: {:?}", start_2.elapsed(), sol_2);
+    let sol_2 = solution_2(&points, &folds);
+    println!("Solved part 2 in {:?}: \n{}", start_2.elapsed(), sol_2);
     assert_eq!(EXP_2, sol_2);
 
     println!("Solved puzzle in {:?}", start.elapsed());
