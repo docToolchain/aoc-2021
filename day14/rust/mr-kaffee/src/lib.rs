0ec4b4c8a704dd9548052f5f6fa36db59f4af95a
Peter Wieland <peter@die-wielands.net>
2021-12-14 08:59:44 +0100

    Solution v1 day14 / rust / mr-kaffee
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day14/rust/mr-kaffee/src/lib.rs
+++ day14/rust/mr-kaffee/src/lib.rs
@@ -1,32 +1,137 @@
+use std::collections::HashMap;
+
 // tag::parse[]
-pub fn parse(content: &str) -> Vec<usize> {
-    content.lines().map(|line| line.parse().expect("Could not parse")).collect()
+pub fn parse(content: &str) -> (Vec<char>, HashMap<(char, char), char>) {
+    let mut parts = content.split("\n\n");
+
+    let template = parts
+        .next()
+        .expect("No template")
+        .chars()
+        .collect::<Vec<_>>();
+
+    let rules = parts
+        .next()
+        .expect("No rules")
+        .lines()
+        .map(|line| {
+            let mut chars = line.chars();
+            let a = chars.next().expect("No first char");
+            let b = chars.next().expect("No second char");
+            let c = chars.skip(4).next().expect("No resulting char");
+            ((a, b), c)
+        })
+        .collect::<HashMap<_, _>>();
+
+    (template, rules)
 }
 // end::parse[]
 
-// tag::part1[]
-pub fn solution_1(data: &[usize]) -> usize {
-    data[0]
-}
-// end::part1[]
+// tag::simulate_rounds[]
+pub fn simulate_rounds(template: &[char], rules: &HashMap<(char, char), char>, rounds: usize) -> usize {
+    let mut counts = HashMap::new();
+    for (c1, c2) in template.iter().zip(template.iter().skip(1)) {
+        let cnt = counts.entry((*c1, *c2)).or_insert(0usize);
+        *cnt += 1;
+    }
 
-// tag::part2[]
-pub fn solution_2(data: &[usize]) -> usize {
-    data[1]
+    for _ in 0..rounds {
+        let mut upd = HashMap::new();
+        for ((c1, c2), cnt0) in &counts {
+            if let Some(d) = rules.get(&(*c1, *c2)) {
+                let cnt = upd.entry((*c1, *d)).or_insert(0);
+                *cnt += cnt0;
+                let cnt = upd.entry((*d, *c2)).or_insert(0);
+                *cnt += cnt0;
+            } else {
+                let cnt = upd.entry((*c1, *c2)).or_insert(0);
+                *cnt += cnt0;
+            }
+        }
+        counts = upd;
+    }
+
+    let mut symbols = HashMap::new();
+    symbols.insert(template[0], 1);
+    symbols.insert(template[template.len() - 1], 1);
+    for ((c1, c2), cnt) in &counts {
+        let count = symbols.entry(*c1).or_insert(0);
+        *count += cnt;
+        let count = symbols.entry(*c2).or_insert(0);
+        *count += cnt;
+    }
+
+    let min = symbols.values().min().expect("No min");
+    let max = symbols.values().max().expect("No max");
+
+    (max - min) / 2
 }
-// end::part2[]
+// end::simulate_rounds[]
+
+pub fn solution_1(template: &[char], rules: &HashMap<(char, char), char>) -> usize {
+    simulate_rounds(template, rules, 10)
+}
+
+pub fn solution_2(template: &[char], rules: &HashMap<(char, char), char>) -> usize {
+    simulate_rounds(template, rules, 40)
+}
 
 // tag::tests[]
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const CONTENT: &str = "0\n1";
-    const DATA: &'static [usize] = &[0, 1];
+    const CONTENT: &str = "NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
+    const TEST_TEMPLATE: [char; 4] = ['N', 'N', 'C', 'B'];
+    const TEST_RULES: [((char, char), char); 16] = [
+        (('C', 'H'), 'B'),
+        (('H', 'H'), 'N'),
+        (('C', 'B'), 'H'),
+        (('N', 'H'), 'C'),
+        (('H', 'B'), 'C'),
+        (('H', 'C'), 'B'),
+        (('H', 'N'), 'C'),
+        (('N', 'N'), 'C'),
+        (('B', 'H'), 'H'),
+        (('N', 'C'), 'B'),
+        (('N', 'B'), 'B'),
+        (('B', 'N'), 'B'),
+        (('B', 'B'), 'N'),
+        (('B', 'C'), 'B'),
+        (('C', 'C'), 'N'),
+        (('C', 'N'), 'C'),
+    ];
 
     #[test]
     fn test_parse() {
-        assert_eq!(DATA, parse(CONTENT));
+        let (template, rules) = parse(&CONTENT);
+        assert_eq!(Vec::from(TEST_TEMPLATE), template);
+        assert_eq!(HashMap::from(TEST_RULES), rules);
+    }
+
+    #[test]
+    fn test_solution_1() {
+        assert_eq!(
+            1_588,
+            solution_1(&Vec::from(TEST_TEMPLATE), &HashMap::from(TEST_RULES))
+        );
     }
 }
 // end::tests[]
--- day14/rust/mr-kaffee/src/main.rs
+++ day14/rust/mr-kaffee/src/main.rs
@@ -4,23 +4,23 @@ use std::time::Instant;
 /// puzzle input
 pub const INPUT: &str = include_str!("../input.txt");
 /// expected result part 1
-pub const EXP_1: usize = 0;
+pub const EXP_1: usize = 2_435;
 /// expected result part 2
-pub const EXP_2: usize = 1;
+pub const EXP_2: usize = 2_587_447_599_164;
 
 fn main() {     
     let start = Instant::now();
-    let data = parse(INPUT);
+    let (template, rules) = parse(INPUT);
 
     // solve part 1
     let start_1 = Instant::now();
-    let sol_1 = solution_1(&data);
+    let sol_1 = solution_1(&template, &rules);
     println!("Solved part 1 in {:?}: {:?}", start_1.elapsed(), sol_1);
     assert_eq!(EXP_1, sol_1);
 
     // solve part 2
     let start_2 = Instant::now();
-    let sol_2 = solution_2(&data);
+    let sol_2 = solution_2(&template, &rules);
     println!("Solved part 2 in {:?}: {:?}", start_2.elapsed(), sol_2);
     assert_eq!(EXP_2, sol_2);
 
