c50f00610ce2c01d20a985bdc8fb9b1ea95b466e
Torsten Sachse <torsten.sachse@gmail.com>
2021-12-16 23:04:39 +0100

    Add very basic file structure

---
+++ day16/go/razziel89/solution.go
@@ -0,0 +1,22 @@
+// Package main is the main executable for razziel89's solution for this day's advent.
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+// tag::solution[]
+
+const (
+	numNeigh = 4
+	replicas = 5
+)
+}
+
+//nolint: funlen
+func main() {
+	fmt.Println("asdf")
+}
+
+// end::solution[]
