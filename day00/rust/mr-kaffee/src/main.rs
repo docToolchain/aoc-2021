d34cc1f797f69dc2dc36272bb24c20a98cfe36e8
Peter Wieland <peter@die-wielands.net>
2021-12-16 21:40:37 +0100

    Run solutions for all days using day 0

--- day00/rust/mr-kaffee/src/main.rs
+++ day00/rust/mr-kaffee/src/main.rs
@@ -1,17 +1,5 @@
 use mr_kaffee_2021_00::*;
-use std::time::Instant;
-use std::fs;
 
-//tag::read_input[]
-fn read_input() -> String {
-    fs::read_to_string("input.txt").expect("Could not read from file")
-}
-//end::read_input[]
-
-//tag::main[]
 fn main() {
-    let instant = Instant::now();
-    write_output(&read_input());
-    println!("Done in {:?}", instant.elapsed());
+    solve();
 }
-//end::main[]
