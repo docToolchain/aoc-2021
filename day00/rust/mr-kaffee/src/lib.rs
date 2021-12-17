7950da56424366e6bf3eb653b3c4a228778cc2be
Peter Wieland <peter@die-wielands.net>
2021-12-17 15:32:33 +0100

    Add day17 to all solutions
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day00/rust/mr-kaffee/src/lib.rs
+++ day00/rust/mr-kaffee/src/lib.rs
@@ -465,15 +465,27 @@ pub mod day16 {
 }
 
 pub mod day17 {
-    // use mr_kaffee_2021_17::*;
+    use mr_kaffee_2021_17::*;
     use std::time::Instant;
 
-    // pub const INPUT: &str = include_str!("../../../../day17/rust/mr-kaffee/input.txt");
-    pub const EXP_1: usize = 0;
-    pub const EXP_2: usize = 0;
-
+    pub const INPUT: &str = include_str!("../../../../day17/rust/mr-kaffee/input.txt");
+    pub const EXP_1: isize = 2_701;
+    pub const EXP_2: usize = 1_070;
+    
     pub fn solve() {
         let timer = Instant::now();
+        let target = parse(INPUT);
+    
+        let timer_1 = Instant::now();
+        let sol_1 = solution_1(target);
+        println!("Solved part 1 in {:?}: {:?}", timer_1.elapsed(), sol_1);
+        assert_eq!(EXP_1, sol_1);
+    
+        let timer_2 = Instant::now();
+        let sol_2 = solution_2(target);
+        println!("Solved part 2 in {:?}: {:?}", timer_2.elapsed(), sol_2);
+        assert_eq!(EXP_2, sol_2);
+    
         println!("-> Solved day 17 in {:?}\n", timer.elapsed());
     }
 }
