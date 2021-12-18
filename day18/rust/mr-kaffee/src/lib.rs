809ccbd7cad88c63e8996c6497d96ff158707a8d
Peter Wieland <peter@die-wielands.net>
2021-12-18 17:34:34 +0100

    Optimizations vec based solution
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day18/rust/mr-kaffee/src/lib.rs
+++ day18/rust/mr-kaffee/src/lib.rs
@@ -48,12 +48,12 @@ pub fn parse(content: &str) -> Vec<Vec<Token>> {
 /// perform single explode
 ///
 /// returns true if any explode performed, otherwise false
-pub fn explode(snailnumber: &mut Vec<Token>) -> bool {
+pub fn explode(snail: &mut Vec<Token>) -> bool {
     let mut level = 0;
     let mut last_val = 0;
     let mut number_seen = false;
-    for k in 0..snailnumber.len() {
-        match snailnumber[k] {
+    for k in 0..snail.len() {
+        match snail[k] {
             Token::Op => {
                 number_seen = false;
                 level += 1;
@@ -67,7 +67,7 @@ pub fn explode(snailnumber: &mut Vec<Token>) -> bool {
                     let prev = (0..k - 2)
                         .rev()
                         .filter_map(|j| {
-                            if let Token::Val(v) = snailnumber[j] {
+                            if let Token::Val(v) = snail[j] {
                                 Some((j, v))
                             } else {
                                 None
@@ -75,12 +75,12 @@ pub fn explode(snailnumber: &mut Vec<Token>) -> bool {
                         })
                         .next();
                     if let Some((j, x)) = prev {
-                        snailnumber[j] = Token::Val(x + last_val);
+                        snail[j] = Token::Val(x + last_val);
                     }
 
-                    let next = (k + 2..snailnumber.len())
+                    let next = (k + 2..snail.len())
                         .filter_map(|j| {
-                            if let Token::Val(v) = snailnumber[j] {
+                            if let Token::Val(v) = snail[j] {
                                 Some((j, v))
                             } else {
                                 None
@@ -88,25 +88,12 @@ pub fn explode(snailnumber: &mut Vec<Token>) -> bool {
                         })
                         .next();
                     if let Some((j, x)) = next {
-                        snailnumber[j] = Token::Val(x + val);
+                        snail[j] = Token::Val(x + val);
                     }
 
-                    assert_eq!(
-                        Token::Cl,
-                        snailnumber.remove(k + 1),
-                        "Expected to remove close"
-                    );
-                    assert_eq!(
-                        Token::Val(val),
-                        snailnumber.remove(k),
-                        "Expected to remove 2nd number"
-                    );
-                    assert_eq!(
-                        Token::Val(last_val),
-                        snailnumber.remove(k - 1),
-                        "Expected to remove 1st number"
-                    );
-                    snailnumber[k - 2] = Token::Val(0);
+                    // remove elements k-1, k, k+1
+                    snail.drain(k - 1..k + 2);
+                    snail[k - 2] = Token::Val(0);
 
                     return true;
                 } else {
@@ -125,8 +112,8 @@ pub fn explode(snailnumber: &mut Vec<Token>) -> bool {
 /// perform single split
 ///
 /// returns true if any split performed, otherwise false
-pub fn split(snailnumber: &mut Vec<Token>) -> bool {
-    let find = snailnumber
+pub fn split(snail: &mut Vec<Token>) -> bool {
+    let find = snail
         .iter()
         .enumerate()
         .filter_map(|(k, e)| {
@@ -143,12 +130,12 @@ pub fn split(snailnumber: &mut Vec<Token>) -> bool {
         .next();
 
     if let Some((k, v)) = find {
-        let a = v / 2;
+        let a = v >> 1;
         let b = v - a;
-        snailnumber[k] = Token::Op;
-        snailnumber.insert(k + 1, Token::Val(a));
-        snailnumber.insert(k + 2, Token::Val(b));
-        snailnumber.insert(k + 3, Token::Cl);
+        snail.splice(
+            k..k + 1,
+            [Token::Op, Token::Val(a), Token::Val(b), Token::Cl],
+        );
 
         true
     } else {
@@ -159,18 +146,18 @@ pub fn split(snailnumber: &mut Vec<Token>) -> bool {
 
 // tag::reduce[]
 /// reduce snail number
-pub fn reduce(snailnumber: &mut Vec<Token>) {
-    while explode(snailnumber) || split(snailnumber) {}
+pub fn reduce(snail: &mut Vec<Token>) {
+    while explode(snail) || split(snail) {}
 }
 // end::reduce[]
 
 // tag::sum[]
 /// calculate sum over snailnumbers at given indices
-pub fn sum(snailnumbers: &[Vec<Token>], mut indices: impl Iterator<Item = usize>) -> Vec<Token> {
-    let mut sum = snailnumbers[indices.next().expect("Empty indices")].to_owned();
+pub fn sum(snail: &[Vec<Token>], mut indices: impl Iterator<Item = usize>) -> Vec<Token> {
+    let mut sum = snail[indices.next().expect("Empty indices")].to_owned();
     for k in indices {
         sum.insert(0, Token::Op);
-        for e in &snailnumbers[k] {
+        for e in &snail[k] {
             sum.push(*e);
         }
         sum.push(Token::Cl);
@@ -184,44 +171,44 @@ pub fn sum(snailnumbers: &[Vec<Token>], mut indices: impl Iterator<Item = usize>
 /// recursively calculate magnitude for snailnumber at given position
 ///
 /// returns magnitude and position
-pub fn magnitude(snailnumber: &[Token], k: usize) -> (usize, usize) {
-    assert_eq!(Token::Op, snailnumber[k], "Do not start with open");
+pub fn magnitude(snail: &[Token], k: usize) -> (usize, usize) {
+    assert_eq!(Token::Op, snail[k], "Do not start with open");
 
-    let (a, j) = match snailnumber[k + 1] {
-        Token::Op => magnitude(&snailnumber, k + 1),
+    let (a, j) = match snail[k + 1] {
+        Token::Op => magnitude(&snail, k + 1),
         Token::Val(v) => (v, k + 2),
         Token::Cl => panic!("Unexpected close tag at {}", k + 1),
     };
-    let (b, j) = match snailnumber[j] {
-        Token::Op => magnitude(&snailnumber, j),
+    let (b, j) = match snail[j] {
+        Token::Op => magnitude(&snail, j),
         Token::Val(v) => (v, j + 1),
         Token::Cl => panic!("Unexpected close tag at {}", j),
     };
 
-    assert_eq!(Token::Cl, snailnumber[j], "Do not end with close");
+    assert_eq!(Token::Cl, snail[j], "Do not end with close");
 
     (3 * a + 2 * b, j + 1)
 }
 // end::magnitude[]
 
 // tag::part1[]
-pub fn solution_1(snailnumbers: &[Vec<Token>]) -> usize {
-    let sum = sum(snailnumbers, 0..snailnumbers.len());
+pub fn solution_1(snails: &[Vec<Token>]) -> usize {
+    let sum = sum(snails, 0..snails.len());
     let (m, _) = magnitude(&sum, 0);
     m
 }
 // end::part1[]
 
 // tag::part2[]
-pub fn solution_2(snailnumbers: &[Vec<Token>]) -> usize {
+pub fn solution_2(snails: &[Vec<Token>]) -> usize {
     let mut max = 0;
-    for k1 in 0..snailnumbers.len() {
-        for k2 in 0..snailnumbers.len() {
+    for k1 in 0..snails.len() {
+        for k2 in 0..snails.len() {
             if k1 == k2 {
                 continue;
             }
 
-            let sum = sum(snailnumbers, [k1, k2].into_iter());
+            let sum = sum(snails, [k1, k2].into_iter());
             let (m, _) = magnitude(&sum, 0);
             if m > max {
                 max = m;
@@ -240,19 +227,19 @@ mod tests {
 
     #[test]
     fn test_explode() {
-        let mut snailnumbers = parse("[[[[[9,8],1],2],3],4]\n[[[[0,9],2],3],4]");
-        assert!(explode(&mut snailnumbers[0]), "Did not explode");
-        assert_eq!(snailnumbers[0], snailnumbers[1]);
+        let mut snails = parse("[[[[[9,8],1],2],3],4]\n[[[[0,9],2],3],4]");
+        assert!(explode(&mut snails[0]), "Did not explode");
+        assert_eq!(snails[0], snails[1]);
 
-        let mut snailnumbers =
+        let mut snails =
             parse("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]\n[[[[0,7],4],[7,[[8,4],9]]],[1,1]]");
-        assert!(explode(&mut snailnumbers[0]));
-        assert_eq!(snailnumbers[0], snailnumbers[1]);
+        assert!(explode(&mut snails[0]));
+        assert_eq!(snails[0], snails[1]);
     }
 
     #[test]
     fn test_split() {
-        let mut snailnumbers = vec![
+        let mut snails = vec![
             vec![
                 Token::Op,
                 Token::Op,
@@ -275,22 +262,22 @@ mod tests {
                 Token::Cl,
             ],
         ];
-        assert!(split(&mut snailnumbers[0]), "Did not split");
-        assert_eq!(snailnumbers[0], snailnumbers[1]);
+        assert!(split(&mut snails[0]), "Did not split");
+        assert_eq!(snails[0], snails[1]);
     }
 
     #[test]
     fn test_reduce() {
-        let mut snailnumbers =
+        let mut snails =
             parse("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]\n[[[[0,7],4],[[7,8],[6,0]]],[8,1]]");
 
-        reduce(&mut snailnumbers[0]);
-        assert_eq!(snailnumbers[0], snailnumbers[1]);
+        reduce(&mut snails[0]);
+        assert_eq!(snails[0], snails[1]);
     }
 
     #[test]
     fn test_sum() {
-        let snailnumbers = parse(
+        let snails = parse(
             "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
 [[[5,[2,8]],4],[5,[[9,9],0]]]
 [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
@@ -303,7 +290,7 @@ mod tests {
 [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]",
         );
 
-        let sum = sum(&snailnumbers, 0..snailnumbers.len());
+        let sum = sum(&snails, 0..snails.len());
         assert_eq!(
             parse("[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]")[0],
             sum
@@ -312,10 +299,10 @@ mod tests {
 
     #[test]
     fn test_magnitude() {
-        let snailnumbers = parse("[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]");
-        let (m, j) = magnitude(&snailnumbers[0], 0);
+        let snails = parse("[[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]");
+        let (m, j) = magnitude(&snails[0], 0);
         assert_eq!(4140, m);
-        assert_eq!(snailnumbers[0].len(), j);
+        assert_eq!(snails[0].len(), j);
     }
 }
 // end::tests[]
