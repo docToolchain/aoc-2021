8ae45375dbda2b422cb4b39530ed750508b4dc0f
Peter Wieland <peter@die-wielands.net>
2021-12-19 22:18:13 +0100

    Optimize day19 / rust / mr-kaffee
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day19/rust/mr-kaffee/src/lib.rs
+++ day19/rust/mr-kaffee/src/lib.rs
@@ -34,7 +34,7 @@ impl CoordMath for Coord {
 
 // tag::trafos[]
 /// transformation functions for all 24 possible transformations
-const TRAFOS: [fn(Coord) -> Coord; 24] = [
+pub const TRAFOS: [fn(Coord) -> Coord; 24] = [
     |(x, y, z)| (x, y, z),
     |(x, y, z)| (x, -y, -z),
     |(x, y, z)| (x, z, -y),
@@ -60,6 +60,34 @@ const TRAFOS: [fn(Coord) -> Coord; 24] = [
     |(x, y, z)| (-z, y, x),
     |(x, y, z)| (-z, -y, -x),
 ];
+
+/// inverse transformation functions for all 24 possible transformations
+pub const INV_TRAFOS: [fn(Coord) -> Coord; 24] = [
+    |(x, y, z)| (x, y, z),
+    |(x, y, z)| (x, -y, -z),
+    |(x, y, z)| (x, -z, y),
+    |(x, y, z)| (x, z, -y),
+    |(x, y, z)| (-x, y, -z),
+    |(x, y, z)| (-x, -y, z),
+    |(x, y, z)| (-x, z, y),
+    |(x, y, z)| (-x, -z, -y),
+    |(x, y, z)| (y, x, -z),
+    |(x, y, z)| (-y, x, z),
+    |(x, y, z)| (z, x, y),
+    |(x, y, z)| (-z, x, -y),
+    |(x, y, z)| (y, -x, z),
+    |(x, y, z)| (-y, -x, -z),
+    |(x, y, z)| (-z, -x, y),
+    |(x, y, z)| (z, -x, -y),
+    |(x, y, z)| (y, z, x),
+    |(x, y, z)| (-y, -z, x),
+    |(x, y, z)| (-z, y, x),
+    |(x, y, z)| (z, -y, x),
+    |(x, y, z)| (y, -z, -x),
+    |(x, y, z)| (-y, z, -x),
+    |(x, y, z)| (z, y, -x),
+    |(x, y, z)| (-z, -y, -x),
+];
 // end::trafos[]
 
 // tag::parse[]
@@ -71,27 +99,19 @@ pub fn parse(content: &str) -> Vec<HashSet<Coord>> {
     let mut scanners = Vec::new();
 
     let mut beacons = HashSet::new();
-    for line in content.lines() {
-        let line = line.trim();
-        if line.len() == 0 {
-            continue;
-        } else if line.starts_with("---") {
+    for line in content.lines().map(|l| l.trim()).filter(|l| l.len() > 0) {
+        if line.starts_with("---") {
             if !beacons.is_empty() {
                 scanners.push(beacons);
                 beacons = HashSet::new();
             }
         } else {
-            let mut parts = line
-                .trim()
-                .split(',')
-                .map(|s| s.parse().expect("Could not parse."));
-            let p = (
-                parts.next().expect("No x"),
-                parts.next().expect("No y"),
-                parts.next().expect("No z"),
-            );
-            assert!(parts.next().is_none(), "More than three coordinates");
-            beacons.insert(p);
+            let mut parts = line.split(',');
+            beacons.insert((
+                parts.next().unwrap().parse().unwrap(),
+                parts.next().unwrap().parse().unwrap(),
+                parts.next().unwrap().parse().unwrap(),
+            ));
         }
     }
     if !beacons.is_empty() {
@@ -105,92 +125,111 @@ pub fn parse(content: &str) -> Vec<HashSet<Coord>> {
 // tag::check_overlap[]
 /// check whether two sets of beacons overlap
 ///
-/// find the transformation ``t`` for the second set, and a distance ``d`` of the references,
-/// so that at least ``threshold`` pairs of beacons (b1, b2) in the two sets exist that satisfy
-/// ``t(b2) - d = b1``
+/// find the transformation ``t`` for the ``beacons_check`` set, and the ``center`` position
+/// of the corresponding scanner in the ``beacons_settled``'s coordinate system so that at
+/// least ``threshold`` pairs of beacons (b1, b2) in the two sets exist that satisfy
+/// ``t(b2) - center = b1``
+///
+/// return the transformation ``t`` and the ``center`` position
 pub fn check_overlap(
-    beacons1: &HashSet<Coord>,
-    beacons2: &HashSet<Coord>,
+    beacons_settled: &HashSet<Coord>,
+    beacons_check: &HashSet<Coord>,
     threshold: usize,
 ) -> Option<(fn(Coord) -> Coord, Coord)> {
-    for t in TRAFOS {
+    for t_idx in 0..24 {
         // find all beacons with the same distance as b1[k1] to b2[k2]
-        for b1 in beacons1 {
-            for b2 in beacons2 {
-                // d = t(b2) - b1
-                // => b1 = t(b2) - d
-                let d = t(*b2).sub(b1);
+        for b1 in beacons_settled {
+            for b2 in beacons_check {
+                // center = t(b2) - b1
+                // => b1 = t(b2) - center
+                let center = TRAFOS[t_idx](*b2).sub(b1);
 
+                // filter for scanner range prior to lookup
+                // settled beacons are in transformed coordinate (scanner is not necessarily at origin)
+                // => need to check range in beacons_check via inverse transform
+                // (possible optimization: use actual bounding box of beacons)
                 let mut count = 0;
-                for _ in beacons2
+                for _ in beacons_settled
                     .iter()
-                    .map(|b2| t(*b2).sub(&d))
-                    .filter(|b2| beacons1.contains(b2))
+                    .map(|b1| INV_TRAFOS[t_idx](b1.add(&center)))
+                    .filter(|(x, y, z)| {
+                        x >= &-1000
+                            && x <= &1000
+                            && y >= &-1000
+                            && y <= &1000
+                            && z >= &-1000
+                            && z <= &1000
+                    })
+                    .filter(|b1| beacons_check.contains(b1))
                 {
                     // don't use iter.count() to avoid checking more elements onece threshold is reached
                     count += 1;
                     if count >= threshold {
-                        return Some((t, d));
+                        return Some((TRAFOS[t_idx], center));
                     }
                 }
             }
         }
     }
 
-    None
+    None // no solution found
 }
 // end::check_overlap[]
 
 // tag::solution[]
-/// check whether any unsettled scanner can be settled given the settled beacons
-///
-/// if so, return the index of that scanner, the beacons in the coordinates of the settled beacons, and the reference distance
-pub fn settle_any(
-    beacons: &HashSet<Coord>,
-    scanners: &[HashSet<Coord>],
-    settled: &[bool],
-) -> Option<(usize, Vec<Coord>, Coord)> {
-    for (k2, s2) in scanners.iter().enumerate().filter(|(k, _)| !settled[*k]) {
-        if let Some((t, d)) = check_overlap(&beacons, s2, 12) {
-            return Some((k2, s2.iter().map(|p| t(*p).sub(&d)).collect(), d));
-        }
-    }
-
-    None
-}
-
-/// determine number of unique beacons
+/// determine number of unique beacons and max Manhatten distance between
+/// any two scanners
 pub fn solution_1_2(scanners: &[HashSet<Coord>]) -> (usize, usize) {
-    // holds flags for settled scanners
-    let mut settled = vec![false; scanners.len()];
-    settled[0] = true; // start with first scanner settled
+    // use my own mutable copy of the scanners
+    let mut scanners = scanners.to_owned();
 
-    // holds distances from all scanners to first scanner.
+    // holds flags for settled scanners
+    // 0 - not settled
+    // 1 - settled, enqueued for furhter extension of settled scanners
+    // 2 - fully processed (dequeued)
+    let mut settled = vec![0; scanners.len()];
+    settled[0] = 1u8; // start with first scanner settled
+
+    // holds scanner coordinates in the coordinate system of the settled scanners.
     // Used to calculate largest distance between any two scanners.
-    let mut deltas = Vec::with_capacity(scanners.len());
-    deltas.push((0, 0, 0));
+    let mut centers = Vec::with_capacity(scanners.len());
+    centers.push((0, 0, 0)); // first scanner's position (= reference)
     let mut max_dist = 0;
 
     // holds all settled beacons. Start with beacons in range of scanner 1
     let mut beacons: HashSet<Coord> = scanners[0].clone();
 
-    // while any scanner has been settled
-    while let Some((k, iter, d)) = settle_any(&beacons, &scanners, &settled) {
-        // update flag
-        settled[k] = true;
-        // add beacons to settled beacons
-        beacons.extend(iter);
+    while let Some(k1) = settled.iter().position(|s| s == &1) {
+        for k2 in 0..settled.len() {
+            if settled[k2] != 0 {
+                continue; // already settled
+            }
 
-        // update max distance
-        for d0 in &deltas {
-            let dist = d.sub(&d0).abs();
-            if dist > max_dist {
-                max_dist = dist;
+            if let Some((t, c)) = check_overlap(&scanners[k1], &scanners[k2], 12) {
+                // update scanner to settled coordinates
+                scanners[k2] = scanners[k2].iter().map(|b| t(*b).sub(&c)).collect();
+
+                // add beacons to unique set
+                beacons.extend(&scanners[k2]);
+
+                // update settled to 1 = enqueue
+                settled[k2] = 1;
+
+                // update max distance
+                for c_settled in &centers {
+                    let dist = c.sub(&c_settled).abs();
+                    if dist > max_dist {
+                        max_dist = dist;
+                    }
+                }
+
+                // add center
+                centers.push(c);
             }
         }
 
-        // save distance to first scanner
-        deltas.push(d);
+        // update settled to 2 = fully processed
+        settled[k1] = 2;
     }
 
     // return number of beacons and largest distance between any two scanners
@@ -228,6 +267,24 @@ mod tests {
     fn test_solution_1_2() {
         let scanners = parse(CONTENT);
         assert_eq!((79, 3621), solution_1_2(&scanners));
+    }
+
+    #[test]
+    fn test_trafos() {
+        for k in 0..24 {
+            assert_eq!(
+                (1, 2, 3),
+                TRAFOS[k](INV_TRAFOS[k]((1, 2, 3))),
+                "Failed trafo after inverse at {}",
+                k
+            );
+            assert_eq!(
+                (1, 2, 3),
+                INV_TRAFOS[k](TRAFOS[k]((1, 2, 3))),
+                "Failed inverse after trafo at {}",
+                k
+            );
+        }
     }
 }
 // end::tests[]
