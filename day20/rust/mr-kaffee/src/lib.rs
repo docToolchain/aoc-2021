5ef5245423ac497cde514519d7f11a273758eb5c
Peter Wieland <peter@die-wielands.net>
2021-12-20 07:27:41 +0100

    Solution v1 day20 / rust / mr-kaffee
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day20/rust/mr-kaffee/src/lib.rs
+++ day20/rust/mr-kaffee/src/lib.rs
@@ -1,23 +1,98 @@
+use std::{cmp, collections::HashSet};
+
+#[derive(Debug, Clone)]
+pub struct Image {
+    pub data: HashSet<(isize, isize)>,
+    pub bbox: (isize, isize, isize, isize),
+    pub neg: bool,
+}
+
 // tag::parse[]
-///
-/// ```
-/// # use mr_kaffee_2021_20::*;
-/// assert_eq!(vec![0, 1], parse("0\n1"));
-/// ```
-pub fn parse(content: &str) -> Vec<usize> {
-    content.lines().map(|line| line.parse().expect("Could not parse")).collect()
+pub fn parse(content: &str) -> (Vec<bool>, Image) {
+    let mut lines = content.lines();
+    let algo = lines
+        .next()
+        .expect("No algo")
+        .trim()
+        .chars()
+        .map(|c| match c {
+            '.' => false,
+            '#' => true,
+            _ => panic!("Illegal character: {}", c),
+        })
+        .collect::<Vec<_>>();
+
+    let mut data = HashSet::new();
+    let mut bbox = (isize::MAX, isize::MAX, isize::MIN, isize::MIN);
+    for (y, line) in lines
+        .map(|line| line.trim())
+        .filter(|line| line.len() > 0)
+        .enumerate()
+    {
+        for (x, _) in line.chars().enumerate().filter(|(_, c)| c == &'#') {
+            data.insert((x as isize, y as isize));
+            bbox.0 = cmp::min(bbox.0, x as isize);
+            bbox.1 = cmp::min(bbox.1, y as isize);
+            bbox.2 = cmp::max(bbox.2, x as isize + 1);
+            bbox.3 = cmp::max(bbox.3, y as isize + 1);
+        }
+    }
+
+    (
+        algo,
+        Image {
+            data,
+            bbox,
+            neg: false,
+        },
+    )
 }
 // end::parse[]
 
+pub fn process(algo: &[bool], image: &Image) -> Image {
+    let mut data = HashSet::new();
+    let mut bbox = (isize::MAX, isize::MAX, isize::MIN, isize::MIN);
+    let neg = algo[0] ^ image.neg;
+
+    for y in image.bbox.1 - 1..image.bbox.3 + 1 {
+        for x in image.bbox.0 - 1..image.bbox.2 + 1 {
+            let mut idx = 0;
+            for dy in -1..=1 {
+                for dx in -1..=1 {
+                    idx =
+                        idx << 1 | ((image.neg ^ image.data.contains(&(x + dx, y + dy))) as usize);
+                }
+            }
+            if neg ^ algo[idx] {
+                data.insert((x, y));
+                bbox.0 = cmp::min(bbox.0, x);
+                bbox.1 = cmp::min(bbox.1, y);
+                bbox.2 = cmp::max(bbox.2, x + 1);
+                bbox.3 = cmp::max(bbox.3, y + 1);
+            }
+        }
+    }
+
+    Image { data, bbox, neg }
+}
+
+pub fn steps(algo: &[bool], image: &Image, steps: usize) -> Image {
+    let mut image = process(&algo, image);
+    for _ in 1..steps {
+        image = process(algo, &image);
+    }
+    image
+}
+
 // tag::part1[]
-pub fn solution_1(data: &[usize]) -> usize {
-    data[0]
+pub fn solution_1(algo: &[bool], image: &Image) -> usize {
+    steps(algo, image, 2).data.len()
 }
 // end::part1[]
 
 // tag::part2[]
-pub fn solution_2(data: &[usize]) -> usize {
-    data[1]
+pub fn solution_2(algo: &[bool], image: &Image) -> usize {
+    steps(algo, image, 50).data.len()
 }
 // end::part2[]
 
@@ -26,12 +101,53 @@ pub fn solution_2(data: &[usize]) -> usize {
 mod tests {
     use super::*;
 
-    const CONTENT: &str = "0\n1";
-    const DATA: &'static [usize] = &[0, 1];
+    const CONTENT: &str =
+        "..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..##\
+        #..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###\
+        .######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#.\
+        .#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#.....\
+        .#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#..\
+        ...####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.....\
+        ..##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+\
+        #..#.\n\
+        #....\n\
+        ##..#\n\
+        ..#..\n\
+        ..###";
 
     #[test]
     fn test_parse() {
-        assert_eq!(DATA, parse(CONTENT));
+        let (algo, image) = parse(CONTENT);
+        assert_eq!(512, algo.len());
+        assert_eq!((0, 0, 5, 5), image.bbox);
+        assert_eq!(
+            HashSet::from([
+                (0, 0),
+                (3, 0),
+                (0, 1),
+                (0, 2),
+                (1, 2),
+                (4, 2),
+                (2, 3),
+                (2, 4),
+                (3, 4),
+                (4, 4)
+            ]),
+            image.data
+        );
+        assert!(!image.neg);
+    }
+
+    #[test]
+    fn test_process() {
+        let (algo, image) = parse(CONTENT);
+        println!("{:?}", image);
+        let image = process(&algo, &image);
+        println!("{:?}", image);
+        let image = process(&algo, &image);
+        println!("{:?}", image);
+        assert_eq!(35, image.data.len());
     }
 }
 // end::tests[]
--- day20/rust/mr-kaffee/src/main.rs
+++ day20/rust/mr-kaffee/src/main.rs
@@ -2,20 +2,20 @@ use mr_kaffee_2021_20::*;
 use std::time::Instant;
 
 pub const INPUT: &str = include_str!("../input.txt");
-pub const EXP_1: usize = 0;
-pub const EXP_2: usize = 1;
+pub const EXP_1: usize = 5_316;
+pub const EXP_2: usize = 16_728;
 
-fn main() {     
+fn main() {
     let timer = Instant::now();
-    let data = parse(INPUT);
+    let (algo, image) = parse(INPUT);
 
     let timer_1 = Instant::now();
-    let sol_1 = solution_1(&data);
+    let sol_1 = solution_1(&algo, &image);
     println!("Solved part 1 in {:?}: {:?}", timer_1.elapsed(), sol_1);
     assert_eq!(EXP_1, sol_1);
 
     let timer_2 = Instant::now();
-    let sol_2 = solution_2(&data);
+    let sol_2 = solution_2(&algo, &image);
     println!("Solved part 2 in {:?}: {:?}", timer_2.elapsed(), sol_2);
     assert_eq!(EXP_2, sol_2);
 
