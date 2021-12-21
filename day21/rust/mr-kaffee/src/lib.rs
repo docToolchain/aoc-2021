29f280f6a97cca022f7df489f5006434ce0fe4e1
Peter Wieland <peter@die-wielands.net>
2021-12-21 10:51:41 +0100

    Nicer looking solution day21
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day21/rust/mr-kaffee/src/lib.rs
+++ day21/rust/mr-kaffee/src/lib.rs
@@ -30,95 +30,90 @@ pub fn parse(content: &str) -> (usize, usize) {
 /// play with a deterministic dice, winning threshold: 1000
 ///
 /// return the looser's score times the number the dice was thrown
-pub fn solution_1((mut pos_1, mut pos_2): (usize, usize)) -> usize {
-    let mut score_1 = 0;
-    let mut score_2 = 0;
+pub fn solution_1((mut p1, mut p2): (usize, usize)) -> usize {
+    let mut s1 = 0;
+    let mut s2 = 0;
 
     let mut dice = 0;
 
-    let mut turn_1 = true;
-    while score_1 < 1000 && score_2 < 1000 {
-        if turn_1 {
-            pos_1 = (pos_1 + 3 * dice + 6 - 1) % 10 + 1;
-            score_1 += pos_1;
+    let mut t = true;
+    while s1 < 1000 && s2 < 1000 {
+        if t {
+            p1 = (p1 + 3 * dice + 6 - 1) % 10 + 1;
+            s1 += p1;
         } else {
-            pos_2 = (pos_2 + 3 * dice + 6 - 1) % 10 + 1;
-            score_2 += pos_2;
+            p2 = (p2 + 3 * dice + 6 - 1) % 10 + 1;
+            s2 += p2;
         }
 
         dice += 3;
-        turn_1 = !turn_1;
+        t = !t;
     }
 
-    dice * cmp::min(score_1, score_2)
+    dice * cmp::min(s1, s2)
 }
 // end::part1[]
 
 // tag::part2_stack[]
+/// 7 possible outcomes with three dice
+///
+/// * 3 - 1: (1 1 1)
+/// * 4 - 3: (1 1 2) (1 2 1) (2 1 1)
+/// * 5 - 6: (1 1 3) (1 2 2) (1 3 1) (2 1 2) (2 2 1) (3 1 1)
+/// * 6 - 7: (1 2 3) (1 3 2) (2 1 3) (2 2 2) (2 3 1) (3 1 2) (3 2 1)
+/// * 7 - 6: (1 3 3) (2 2 3) (2 3 2) (3 1 3) (3 2 2) (3 3 1)
+/// * 8 - 3: (2 3 3) (3 2 3) (3 3 2)
+/// * 9 - 1: (3 3 3)
+pub const DICE_MULTS: [(usize, usize); 7] =
+    [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)];
+
 #[cfg(feature = "stack")]
 /// play with dirac quantum dice (stack based)
 ///
 /// return the number of universes won by the player who wins more often
-pub fn solution_2((pos_1, pos_2): (usize, usize)) -> usize {
-    // 7 possible outcomes with three dice
-    // 3 - 1      (1 1 1)
-    // 4 - 3      (1 1 2) (1 2 1) (2 1 1)
-    // 5 - 6      (1 1 3) (1 2 2) (1 3 1) (2 1 2) (2 2 1) (3 1 1)
-    // 6 - 7      (1 2 3) (1 3 2) (2 1 3) (2 2 2) (2 3 1) (3 1 2) (3 2 1)
-    // 7 - 6      (1 3 3) (2 2 3) (2 3 2) (3 1 3) (3 2 2) (3 3 1)
-    // 8 - 3      (2 3 3) (3 2 3) (3 3 2)
-    // 9 - 1      (3 3 3)
-
+pub fn solution_2((p1, p2): (usize, usize)) -> usize {
     // push states with multiplicty on stack
     // since I push to the end and pop from the end, this is depth first search
     let mut stack = Vec::new();
-    stack.push((pos_1, pos_2, 0, 0, 1, true));
+    stack.push((p1, p2, 0, 0, true, 1));
 
-    // counters for wins
-    let mut wins_1 = 0;
-    let mut wins_2 = 0;
-
-    let mut max_stack_size = 1;
-    let mut rounds = 0;
+    // win counts
+    let mut w1 = 0;
+    let mut w2 = 0;
 
     // while there are unprocessed states
-    while let Some((pos_1, pos_2, score_1, score_2, mult_state, turn_1)) = stack.pop() {
+    while let Some((p1, p2, s1, s2, t, mult)) = stack.pop() {
         // loop over dice outcomes with multiplicity
-        for (dice, mult_dice) in [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)] {
-            if turn_1 {
+        for (v, n) in DICE_MULTS {
+            if t {
                 // player 1's turn
                 // update position and score
-                let pos = (pos_1 + dice - 1) % 10 + 1;
-                let score = score_1 + pos;
-                if score >= 21 {
+                let p1_upd = (p1 + v - 1) % 10 + 1;
+                let s1_upd = s1 + p1_upd;
+                if s1_upd >= 21 {
                     // win
-                    wins_1 += mult_dice * mult_state;
+                    w1 += n * mult;
                 } else {
                     // continue to play later
-                    stack.push((pos, pos_2, score, score_2, mult_dice * mult_state, false));
+                    stack.push((p1_upd, p2, s1_upd, s2, false, n * mult));
                 }
             } else {
                 // player 2's turn
                 // update position and score
-                let pos = (pos_2 + dice - 1) % 10 + 1;
-                let score = score_2 + pos;
-                if score >= 21 {
+                let p2_upd = (p2 + v - 1) % 10 + 1;
+                let s2_upd = s2 + p2_upd;
+                if s2_upd >= 21 {
                     // win
-                    wins_2 += mult_dice * mult_state;
+                    w2 += n * mult;
                 } else {
                     // continue to play later
-                    stack.push((pos_1, pos, score_1, score, mult_dice * mult_state, true));
+                    stack.push((p1, p2_upd, s1, s2_upd, true, n * mult));
                 }
             }
         }
-
-        max_stack_size = cmp::max(max_stack_size, stack.len());
-        rounds += 1;
     }
 
-    println!("Max stack size in {} rounds: {}", rounds, max_stack_size);
-
-    cmp::max(wins_1, wins_2)
+    cmp::max(w1, w2)
 }
 // end::part2_stack[]
 
@@ -127,53 +122,49 @@ pub fn solution_2((pos_1, pos_2): (usize, usize)) -> usize {
 /// play with dirac quantum dice (state list based)
 ///
 /// return the number of universes won by the player who wins more often
-pub fn solution_2((pos_1, pos_2): (usize, usize)) -> usize {
-    let f_idx = |pos_1: usize, pos_2: usize, score_1: usize, score_2: usize, turn: usize| {
-        pos_1 + 10 * pos_2 + 100 * score_1 + 2100 * score_2 + 44100 * turn
-    };
+pub fn solution_2((p1, p2): (usize, usize)) -> usize {
+    // index to state multiplicity list
+    const F_IDX: fn(usize, usize, usize, usize, usize) -> usize =
+        |p1, p2, s1, s2, t| p1 + 10 * p2 + 100 * s1 + 2100 * s2 + 44100 * t;
 
-    let mut state_mults = vec![0; 10 * 10 * 21 * 21 * 2];
-    // insert at pos_1 - 1 and pos_2 - 1 because I use zero based position internally
-    state_mults[f_idx(pos_1 - 1, pos_2 - 1, 0, 0, 0)] = 1;
+    // state multiplicity list
+    let mut mults = vec![0; 10 * 10 * 21 * 21 * 2];
 
-    let mut wins_1 = 0;
-    let mut wins_2 = 0;
+    // insert at pos_1 - 1 and pos_2 - 1 because position is zero based internally
+    mults[F_IDX(p1 - 1, p2 - 1, 0, 0, 0)] = 1;
 
-    for sum_score in 0..=40 {
-        for score_1 in 0..=cmp::min(20, sum_score) {
-            let score_2 = sum_score - score_1;
-            if score_2 > 20 {
-                continue;
-            }
+    // win counts
+    let mut w1 = 0;
+    let mut w2 = 0;
 
-            for pos_1 in 0..10 {
-                for pos_2 in 0..10 {
-                    let state_mult_1 = state_mults[f_idx(pos_1, pos_2, score_1, score_2, 0)];
-                    let state_mult_2 = state_mults[f_idx(pos_1, pos_2, score_1, score_2, 1)];
+    for sum in 0..=40 {
+        for s1 in if sum > 20 { sum - 20..=20 } else { 0..=sum } {
+            let s2 = sum - s1;
 
-                    if state_mult_1 == 0 && state_mult_2 == 0 {
+            for p1 in 0..10 {
+                for p2 in 0..10 {
+                    let mult_1 = mults[F_IDX(p1, p2, s1, s2, 0)];
+                    let mult_2 = mults[F_IDX(p1, p2, s1, s2, 1)];
+
+                    if mult_1 == 0 && mult_2 == 0 {
                         continue; // don't loop if no need
                     }
 
-                    for (dice, dice_mult) in
-                        [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)]
-                    {
-                        let pos_1_upd = (pos_1 + dice) % 10;
-                        let score_1_upd = score_1 + pos_1_upd + 1;
-                        if score_1_upd >= 21 {
-                            wins_1 += state_mult_1 * dice_mult;
+                    for (v, n) in DICE_MULTS {
+                        let p1_upd = (p1 + v) % 10;
+                        let s1_upd = s1 + p1_upd + 1;
+                        if s1_upd >= 21 {
+                            w1 += mult_1 * n;
                         } else {
-                            state_mults[f_idx(pos_1_upd, pos_2, score_1_upd, score_2, 1)] +=
-                                state_mult_1 * dice_mult;
+                            mults[F_IDX(p1_upd, p2, s1_upd, s2, 1)] += mult_1 * n;
                         }
 
-                        let pos_2_upd = (pos_2 + dice) % 10;
-                        let score_2_upd = score_2 + pos_2_upd + 1;
-                        if score_2_upd >= 21 {
-                            wins_2 += state_mult_2 * dice_mult;
+                        let p2_upd = (p2 + v) % 10;
+                        let s2_upd = s2 + p2_upd + 1;
+                        if s2_upd >= 21 {
+                            w2 += mult_2 * n;
                         } else {
-                            state_mults[f_idx(pos_1, pos_2_upd, score_1, score_2_upd, 0)] +=
-                                state_mult_2 * dice_mult;
+                            mults[F_IDX(p1, p2_upd, s1, s2_upd, 0)] += mult_2 * n;
                         }
                     }
                 }
@@ -181,7 +172,7 @@ pub fn solution_2((pos_1, pos_2): (usize, usize)) -> usize {
         }
     }
 
-    cmp::max(wins_1, wins_2)
+    cmp::max(w1, w2)
 }
 // end::part2_list[]
 
