51880d4287c8b5ba680d4711e4bc8ed56a663a4a
Peter Wieland <peter@die-wielands.net>
2021-12-23 18:36:41 +0100

    Solution with settle, passes all tests but fails for real data part 1
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day23/rust/mr-kaffee/src/lib.rs
+++ day23/rust/mr-kaffee/src/lib.rs
@@ -2,6 +2,7 @@
 use std::{
     collections::{BTreeSet, HashMap, HashSet},
     fmt,
+    hash::Hash,
 };
 
 #[derive(PartialEq, Eq, Clone, Copy, PartialOrd, Ord, Hash)]
@@ -414,26 +415,29 @@ impl Burrow {
 }
 
 #[derive(Debug)]
-pub struct Search {
-    heap: BTreeSet<(usize, Burrow)>,
-    settled: HashSet<Burrow>,
-    costs: HashMap<Burrow, usize>,
+pub struct Search<T> {
+    heap: BTreeSet<(usize, T)>,
+    settled: HashSet<T>,
+    costs: HashMap<T, usize>,
 }
 
-impl Search {
-    pub fn init(burrow: Burrow) -> Self {
+impl<T> Search<T>
+where
+    T: Eq + Ord + Hash + Copy,
+{
+    pub fn init(cost: usize, burrow: T) -> Self {
         let mut search = Self {
             heap: BTreeSet::new(),
             settled: HashSet::new(),
             costs: HashMap::new(),
         };
-        search.heap.insert((0, burrow));
+        search.heap.insert((cost, burrow));
         search.settled.insert(burrow);
-        search.costs.insert(burrow, 0);
+        search.costs.insert(burrow, cost);
         search
     }
 
-    pub fn pop(&mut self) -> Option<(usize, Burrow)> {
+    pub fn pop(&mut self) -> Option<(usize, T)> {
         if let Some((weight, burrow)) = self.heap.pop_first() {
             self.settled.insert(burrow);
             Some((weight, burrow))
@@ -442,21 +446,21 @@ impl Search {
         }
     }
 
-    pub fn push(&mut self, cost: usize, weight: usize, burrow: Burrow) -> bool {
-        if self.settled.contains(&burrow) {
+    pub fn push(&mut self, cost: usize, adjacent: T) -> bool {
+        if self.settled.contains(&adjacent) {
             return false;
         }
 
-        if let Some(cur_cost) = self.costs.get(&burrow) {
-            if cost + weight < *cur_cost {
-                self.heap.remove(&(*cur_cost, burrow));
+        if let Some(cur_cost) = self.costs.get(&adjacent) {
+            if cost < *cur_cost {
+                self.heap.remove(&(*cur_cost, adjacent));
             } else {
                 return false;
             }
         }
 
-        self.heap.insert((cost + weight, burrow));
-        self.costs.insert(burrow, cost + weight);
+        self.heap.insert((cost, adjacent));
+        self.costs.insert(adjacent, cost);
 
         true
     }
@@ -469,7 +473,7 @@ pub fn parse(content: &str) -> Burrow {
 // end::parse[]
 
 pub fn solution_1(burrow: Burrow) -> usize {
-    solve(
+    solve1(
         burrow,
         Burrow {
             data: [
@@ -497,9 +501,91 @@ pub fn solution_1(burrow: Burrow) -> usize {
     )
 }
 
-// tag::part1[]
-pub fn solve(burrow: Burrow, target: Burrow) -> usize {
-    let mut search = Search::init(burrow);
+pub fn settle1(burrow: Burrow) -> Option<(Burrow, usize)> {
+    let mut update = burrow;
+    let mut weight = 0;
+
+    for k in 0..Burrow::LEN {
+        if let Some(p) = burrow.data[k] {
+            let cur_room = if k >= Burrow::HALLWAY_LEN {
+                ((k - Burrow::HALLWAY_LEN) / 2) as u8
+            } else {
+                u8::MAX
+            };
+
+            if cur_room == p {
+                // already in own room -> cannot settle
+                continue;
+            }
+
+            if burrow.data[Burrow::ROOMS_1[p as usize]].is_some() {
+                // no free space in room
+                continue;
+            }
+
+            if let Some(p2) = burrow.data[Burrow::ROOMS_2[p as usize]] {
+                if p2 != p {
+                    // room is occupied by foreigner
+                    continue;
+                }
+            }
+
+            let mut done = [false; Burrow::LEN];
+            let mut stack = Vec::new();
+
+            done[k] = true;
+            for k_adj in Burrow::ADJ[k] {
+                done[*k_adj] = true;
+                stack.push((1, *k_adj));
+            }
+
+            while let Some((steps, k_adj)) = stack.pop() {
+                if burrow.data[k_adj].is_some() {
+                    // cannot move on top of other
+                    continue;
+                }
+
+                let adj_room = if k_adj >= Burrow::HALLWAY_LEN {
+                    ((k_adj - Burrow::HALLWAY_LEN) / 2) as u8
+                } else {
+                    u8::MAX
+                };
+
+                if adj_room == p
+                    && (k_adj + 1..=Burrow::ROOMS_2[p as usize])
+                        .all(|k_r| burrow.data[k_r] == Some(p))
+                {
+                    // all below are settled
+                    update.data[k] = None;
+                    update.data[k_adj] = Some(p);
+                    weight += steps * Burrow::ENERGIES[p as usize];
+                }
+
+                for k_adj_2 in Burrow::ADJ[k_adj] {
+                    if !done[*k_adj_2] {
+                        done[*k_adj_2] = true;
+                        stack.push((steps + 1, *k_adj_2));
+                    }
+                }
+            }
+        }
+    }
+
+    if weight > 0 {
+        Some((update, weight))
+    } else {
+        None
+    }
+}
+
+pub fn solve1(burrow: Burrow, target: Burrow) -> usize {
+    let mut burrow = burrow;
+    let mut cost = 0;
+    while let Some((update, weight)) = settle1(burrow) {
+        burrow = update;
+        cost += weight;
+    }
+    let mut search = Search::init(cost, burrow);
 
     while let Some((cost, burrow)) = search.pop() {
         if burrow == target {
@@ -508,6 +594,19 @@ pub fn solve(burrow: Burrow, target: Burrow) -> usize {
 
         for k in 0..Burrow::LEN {
             if let Some(p) = burrow.data[k] {
+                let cur_room = if k >= Burrow::HALLWAY_LEN {
+                    ((k - Burrow::HALLWAY_LEN) / 2) as u8
+                } else {
+                    u8::MAX
+                };
+
+                if cur_room == p
+                    && (k + 1..=Burrow::ROOMS_2[p as usize]).all(|k_r| burrow.data[k_r] == Some(p))
+                {
+                    // position is settled in room and below are only the right ones
+                    continue;
+                }
+
                 let mut done = [false; Burrow::LEN];
                 let mut stack = Vec::new();
 
@@ -523,16 +622,56 @@ pub fn solve(burrow: Burrow, target: Burrow) -> usize {
                         continue;
                     }
 
-                    // don't create adjacent in foreign room but continue to look around. I might be moving out
-                    let foreign_room = k_adj >= Burrow::HALLWAY_LEN
-                        && (k_adj - Burrow::HALLWAY_LEN) / 2 != p as usize;
+                    let adj_room = if k_adj >= Burrow::HALLWAY_LEN {
+                        ((k_adj - Burrow::HALLWAY_LEN) / 2) as u8
+                    } else {
+                        u8::MAX
+                    };
 
-                    if !Burrow::DOORS.contains(&k_adj) && !foreign_room {
+                    let own_room = adj_room < u8::MAX && adj_room == p;
+                    if own_room && cur_room != p {
+                        // move in own room
+
+                        if (k_adj + 1..Burrow::ROOMS_2[p as usize])
+                            .any(|k_r| burrow.data[k_r].map(|p_2| p_2 != p).unwrap_or(false))
+                        {
+                            // foreign pods are in room
+                            continue;
+                        }
+
+                        if (k_adj + 1..Burrow::ROOMS_2[p as usize])
+                            .all(|k_r| burrow.data[k_r] == Some(p))
+                        {
+                            // all below are settled
+                            let mut adjacent = burrow.clone();
+                            adjacent.data[k] = None;
+                            adjacent.data[k_adj] = Some(p);
+                            let mut cost = cost + steps * Burrow::ENERGIES[p as usize];
+                            while let Some((update, weight)) = settle1(adjacent) {
+                                cost += weight;
+                                adjacent = update;
+                            }
+                            search.push(cost, adjacent);
+                        }
+                        // otherwise do not create an adjacent but continue to look ahaed
+                    } else if adj_room != u8::MAX {
+                        // move out of room
+                        if cur_room != adj_room || done[k_adj - 1] {
+                            // moving in
+                            continue;
+                        }
+                        // don't create adjacent in foreign room but continue to look ahead
+                    } else if !Burrow::DOORS.contains(&k_adj) {
                         // if I am not in front of a door, this is a valid adjacent
                         let mut adjacent = burrow.clone();
                         adjacent.data[k] = None;
                         adjacent.data[k_adj] = Some(p);
-                        search.push(cost, steps * Burrow::ENERGIES[p as usize], adjacent);
+                        let mut cost = cost + steps * Burrow::ENERGIES[p as usize];
+                        while let Some((update, weight)) = settle1(adjacent) {
+                            cost += weight;
+                            adjacent = update;
+                        }
+                        search.push(cost, adjacent);
                     }
 
                     for k_adj_2 in Burrow::ADJ[k_adj] {
@@ -548,13 +687,62 @@ pub fn solve(burrow: Burrow, target: Burrow) -> usize {
 
     panic!("No path found");
 }
-// end::part1[]
 
 // tag::part2[]
 pub fn solution_2(_burrow: Burrow) -> usize {
-    1
+    todo!()
 }
-// end::part2[]
+
+pub fn solve2(burrow: Burrow2, target: Burrow2) -> usize {
+    let mut search = Search::init(0, burrow);
+
+    while let Some((cost, burrow)) = search.pop() {
+        if burrow == target {
+            return cost; // target reached
+        }
+
+        for k in 0..Burrow2::LEN {
+            if let Some(p) = burrow.data[k] {
+                let mut done = [false; Burrow2::LEN];
+                let mut stack = Vec::new();
+
+                done[k] = true;
+                for k_adj in Burrow2::ADJ[k] {
+                    done[*k_adj] = true;
+                    stack.push((1, *k_adj));
+                }
+
+                while let Some((steps, k_adj)) = stack.pop() {
+                    if burrow.data[k_adj].is_some() {
+                        // cannot move on top of other
+                        continue;
+                    }
+
+                    // don't create adjacent in foreign room but continue to look around. I might be moving out
+                    let foreign_room = k_adj >= Burrow2::HALLWAY_LEN
+                        && (k_adj - Burrow2::HALLWAY_LEN) / 4 != p as usize;
+
+                    if !Burrow2::DOORS.contains(&k_adj) && !foreign_room {
+                        // if I am not in front of a door, this is a valid adjacent
+                        let mut adjacent = burrow.clone();
+                        adjacent.data[k] = None;
+                        adjacent.data[k_adj] = Some(p);
+                        search.push(cost + steps * Burrow2::ENERGIES[p as usize], adjacent);
+                    }
+
+                    for k_adj_2 in Burrow2::ADJ[k_adj] {
+                        if !done[*k_adj_2] {
+                            done[*k_adj_2] = true;
+                            stack.push((steps + 1, *k_adj_2));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    panic!("No path found");
+}
 
 // tag::tests[]
 #[cfg(test)]
@@ -666,7 +854,63 @@ mod tests {
   #########";
 
     #[test]
+    fn test_solve1() {
+        let burrows = [
+            parse(CONTENT_1),
+            parse(CONTENT_2),
+            parse(CONTENT_3),
+            parse(CONTENT_4),
+            parse(CONTENT_5),
+            parse(CONTENT_6),
+            parse(CONTENT_7),
+            parse(CONTENT_8),
+        ];
+
+        let exp_costs = [40, 400, 3030, 40, 2003, 7000, 8, 0];
+
+        for k1 in 0..burrows.len() - 1 {
+            let start = burrows[k1];
+            let mut target = burrows[k1 + 1].clone();
+            let mut exp_cost = exp_costs[k1];
+            while let Some((update, weight)) = settle1(target) {
+                exp_cost += weight;
+                target = update;
+            }
+            let act_cost = solve1(start, target);
+            assert_eq!(exp_cost, act_cost);
+        }
+
+        let mut start = burrows[0];
+        let mut act_cost = 0;
+        for k in 1..burrows.len() {
+            let mut target = burrows[k];
+            let mut exp_adder = 0;
+            while let Some((update, weight)) = settle1(target) {
+                exp_adder += weight;
+                target = update;
+            }
+            let weight = solve1(start, target);
+            act_cost += weight;
+            start = target;
+            let exp_cost = exp_costs.iter().take(k).sum::<usize>();
+            assert_eq!(exp_cost + exp_adder, act_cost);
+        }
+        assert_eq!(exp_costs.iter().sum::<usize>(), act_cost);
+    }
+
+    #[test]
     fn test_solution_1() {
+        let costs = [
+            40 + 400 + 3030 + 40 + 2003 + 7000 + 8,
+            400 + 3030 + 40 + 2003 + 7000 + 8,
+            3030 + 40 + 2003 + 7000 + 8,
+            40 + 2003 + 7000 + 8,
+            2003 + 7000 + 8,
+            7000 + 8,
+            8,
+            0,
+        ];
+
         let burrows = [
             BURROW_1,
             parse(CONTENT_2),
@@ -678,31 +922,65 @@ mod tests {
             parse(CONTENT_8),
         ];
 
-        let costs = [
-            8 + 7000 + 2003 + 40 + 3030 + 400 + 40,
-            8 + 7000 + 2003 + 40 + 3030 + 400,
-            8 + 7000 + 2003 + 40 + 3030,
-            8 + 7000 + 2003 + 40,
-            8 + 7000 + 2003,
-            8 + 7000,
-            8,
-            0,
-        ];
+        for k in (0..1).rev() {
+            println!("{}", k);
+            let cost = solution_1(burrows[k]);
 
-        println!("Test is target");
-        assert!(burrows.last().unwrap().is_target());
+            // println!("\n=============\n=============\n");
+            // let mut target = Burrow::parse(CONTENT_8);
+            // println!("{:?}", target);
+            // while let Some((burrow, cost)) = parents.get(&target) {
+            //     println!("{:?} reaches parent at {}", burrow, cost);
+            //     target = *burrow;
+            // }
 
-        println!("5 to 6: {}", solve(burrows[4], burrows[5]));
-
-        for (k, burrow) in burrows.iter().enumerate().rev() {
-            println!("Test {}\n{:?}", k + 1, burrow);
-            assert_eq!(costs[k], solution_1(burrows[k]));
+            assert_eq!(costs[k], cost);
         }
     }
 
     #[test]
     fn test_burrow2_from() {
         assert_eq!(BURROW_2, Burrow2::from(BURROW_1));
+    }
+
+    #[test]
+    fn test_step_1() {
+        let start = parse(
+            "#############
+#.....D...A.#
+###.#B#C#.###
+  #A#B#C#D#
+  #########",
+        );
+        let target = parse(
+            "#############
+#...........#
+###A#B#C#D###
+  #A#B#C#D#
+  #########",
+        );
+        let cost = solve1(start, target);
+        println!("{}", cost);
+    }
+
+    #[test]
+    fn test_step_2() {
+        let start = parse(
+            "#############
+#.....D.....#
+###.#B#C#D###
+  #A#B#C#A#
+  #########",
+        );
+        let target = parse(
+            "#############
+#...........#
+###A#B#C#D###
+  #A#B#C#D#
+  #########",
+        );
+        let cost = solve1(start, target);
+        assert_eq!(9011, cost);
     }
 }
 // end::tests[]
