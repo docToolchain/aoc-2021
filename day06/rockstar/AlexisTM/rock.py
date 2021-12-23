8ceed13ec5d65b67f57ff0f9afdc03953549c3b6
Alexis Paques <alexis.paques@gmail.com>
2021-12-23 12:33:46 +0100

    Formatting

--- day06/rockstar/AlexisTM/rock.py
+++ day06/rockstar/AlexisTM/rock.py
@@ -1,29 +1,31 @@
 # (After 16 iterations hashtable)
 HashTable16 = [
     "50027",
-     "61138",
-     "0224",
-     "1335",
-     "2446",
-     "3557",
-     "4668",
-     "507",
-     "618"
+    "61138",
+    "0224",
+    "1335",
+    "2446",
+    "3557",
+    "4668",
+    "507",
+    "618"
 ]
 
 file = open("input.txt", "r")
 val_input = [int(num) for num in file.readline().split(",")]
 
+
 def QuickChange(array):
     iter = []
     for val in array:
-    # while len(array) > 0:
-    #    val = array.pop()
+        # while len(array) > 0:
+        #    val = array.pop()
         iter.append(HashTable16[int(val)])
     iter = "".join(iter)
     return iter
 
-def ShopPreparation(max = 8):
+
+def ShopPreparation(max=8):
     shop = [0]*(max+1)
     while max >= 0:
         iteration = [max]
@@ -36,8 +38,9 @@ def ShopPreparation(max = 8):
         iteration = QuickChange(iteration)
         iteration = QuickChange(iteration)
         shop[max] = iteration
-        max = max -1
+        max = max - 1
     return shop
+
 
 def Reflect(shop, max):
     final_hashtable = [0]*(max+1)
@@ -49,8 +52,9 @@ def Reflect(shop, max):
             total = total + len(shop[ival])
 
         final_hashtable[max] = total
-        max = max -1
+        max = max - 1
     return final_hashtable
+
 
 shop = ShopPreparation(8)
 final_hashtable = Reflect(shop, 5)
