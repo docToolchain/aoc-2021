2b0507604fabccabb1c7e70880edde72b2c94a5c
RJPlog <tfrenz67@gmail.com>
2023-12-17 14:36:14 +0100

    Update day2115_1_2.kts
    Algorithm of Dijkstra implemented. Part 2 now is also working

--- day15/kotlin/RJPlog/day2115_1_2.kts
+++ day15/kotlin/RJPlog/day2115_1_2.kts
@@ -1,96 +1,99 @@
 import java.io.File
+import kotlin.math.*
 
-// tag::followPath_15[]
-fun followPath_15_test(input1: Int): Int {
+// tag::HillClimbing[]
+fun HillClimbing(in1: Int): Int {
 
-//	var ruleCheckPassed: Boolean = false
+	var landscape: String = ""
+	var xStart: Int = 0
+	var yStart: Int = 0
+
 	var width: Int = 0
 	var height: Int = 0
-	var riskLevel = mutableListOf<Int>()
-	var riskLevelSum = mutableListOf<Int>()
-
-	for (j in 0..input1 - 1) {
+	
+		var riskLevel = mutableListOf<Int>()
+	
+	for (j in 0..in1 - 1) {
 		File("day2115_puzzle_input.txt").forEachLine {
-			width = it.length * input1
-			for (i in 0..input1 - 1) {
+			width = it.length * in1
+			for (i in 0..in1 - 1) {
 				it.forEach {
 					var risk = it.toString().toInt() + i + j
 					if (risk > 9) {
 						risk = risk % 9
 					}
 					riskLevel.add(risk)
-					riskLevelSum.add(risk)
+					
 				}
 			}
 			height += 1
 		}
 	}
-
+	
 	println("width $width, height $height")
-/*	println("risklevel: $riskLevel")
-	println("risklevelSum: $riskLevelSum")
-	for (y in 0..height - 1) {
-		for (x in 0..width - 1) {
-			print(riskLevel[x + y * width])
-		}
-		println()
-	} */
+	
+	var xEnd: Int = width-1
+	var yEnd: Int = height-1
+	
 
-	println("next")
-	// create riskLevelSum
+	var distList = MutableList(width * height) { width * height * 9}
+	distList[xStart * yStart * width] = 0
 
-	for (y in 0..height - 1) {
-		for (x in 0..width - 1) {
-			if (!(x == 0 && y == 0)) {
-				if (x == 0) {
-					riskLevelSum[x + y * width] = riskLevel[x + y * width] + riskLevelSum[(y - 1) * width]
-				} else if (y == 0) {
-					riskLevelSum[x + y * width] = riskLevel[x + y * width] + riskLevelSum[(x - 1)]
-				} else {
-					riskLevelSum[x + y * width] = riskLevel[x + y * width] + minOf(
-						riskLevelSum[(x - 1) + y * width],
-						riskLevelSum[x + (y - 1) * width]
-					)
+	// iterate over all points
+	var gameEnd: Boolean = false
+	var distListSum = distList.sum()
+
+	while (!gameEnd) {
+		for (y in 0..height - 1) {
+			for (x in 0..width - 1) {
+				// check successors of all known nodes
+				var dist = distList[x + y * width]
+				if (dist != width * height) {
+					// calculate all possible directions
+					if (x - 1 >= 0) {
+							distList[x - 1 + y * width] = min(distList[(x - 1) + y * width], dist + riskLevel[(x-1) + y * width])
+					}
+					if (x + 1 < width) {
+							distList[(x + 1) + y * width] = min(distList[(x + 1) + y * width], dist + riskLevel[(x+1) + y * width])
+					}
+					if (y - 1 >= 0) {
+							distList[x + (y - 1) * width] = min(distList[x + (y - 1) * width], dist + riskLevel[x + (y-1) * width])
+					}
+					if (y + 1 < height) {
+							distList[x + (y + 1) * width] = min(distList[x + (y + 1) * width], dist + riskLevel[x + (y+1) * width])
+					}
 				}
-				//	println(riskLevelSum[x+y*width])
 			}
 		}
-
+		if (distListSum == distList.sum()) {
+			gameEnd = true
+		}
+		distListSum = distList.sum()
 	}
-//	println("riskLevelSum: $riskLevelSum")
-
-	println("riskLevelSum.size ${riskLevelSum.size}")
-	println("${riskLevelSum.last()}, ${riskLevelSum.first()}")
-
-
-	return (riskLevelSum.last() - riskLevelSum.first())
+	return distList[xEnd + yEnd * width]
 }
-// end::followPath_15[]
+// end::Hillclimbing[]
 
-
-fun main(args: Array<String>) {
+fun main() {
 	var t1 = System.currentTimeMillis()
 
+	var solution1 = HillClimbing(1)
+	var solution2 = HillClimbing(5)
 
-	var solution1 = followPath_15_test(1)
-	println()
-
-	var solution2 = followPath_15_test(5)
-
-	// tag::output[]
+// tag::output[]
 // print solution for part 1
-	println("***********************")
-	println("--- Day 15: Chiton  ---")
-	println("***********************")
+	println("***************************************")
+	println("--- Day 12: Hill Climbing Algorithm ---")
+	println("***************************************")
 	println("Solution for part1")
-	println("   $solution1 ")
+	println("   $solution1 is the fewest steps.")
 	println()
 // print solution for part 2
-	println("*******************************")
+	println("***************************************")
 	println("Solution for part2")
-	println("   $solution2 ")   //3001 is to high
-	println()
+	println("   $solution2 is the fewest steps.")
+// end::output[]
+
 	t1 = System.currentTimeMillis() - t1
 	println("puzzle solved in ${t1} ms")
-// end::output[]
 }
